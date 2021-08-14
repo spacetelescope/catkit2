0a1898ccd164930c59a7a91eb1c315f005f6e045
Emiel Por <epor@stsci.edu>
2021-08-14 10:52:51 -0700

    Add hostname.

---
+++ lib/catkit-core/HostName.cpp
@@ -0,0 +1,36 @@
+#include "HostName.h"
+
+#include <cstring>
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <tchar.h>
+#else
+#include <unistd.h>
+#endif
+
+void GetHostName(char* hostname)
+{
+	char name[150];
+
+#ifdef _WIN32
+	TCHAR info_buf[150];
+	DWORD buf_char_count = 150;
+	memset(name, 0, 150);
+	if (GetComputerName(info_buf, &buf_char_count))
+	{
+		for (int i = 0; i<150; i++)
+		{
+			name[i] = info_buf[i];
+		}
+	}
+	else
+	{
+		strcpy(name, "unknown");
+	}
+#else
+	memset(name, 0, 150);
+	gethostname(name, 150);
+#endif
+	strncpy(hostname, name, 150);
+}
---
+++ lib/catkit-core/include/HostName.h
@@ -0,0 +1,6 @@
+#ifndef HOSTNAME_H
+#define HOSTNAME_H
+
+void GetHostName(char* hostname);
+
+#endif // HOSTNAME_H
