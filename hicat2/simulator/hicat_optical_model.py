0020b2b1663f854b6ef55cac58611483495b8c00
Emiel Por <epor@stsci.edu>
2021-11-24 22:56:29 -0500

    Add skeleton for hicat optical model.

---
+++ hicat2/simulator/hicat_optical_model.py
@@ -0,0 +1,90 @@
+from .optical_model import *
+
+class HicatOpticalModel(OpticalModel):
+    def __init__(self, with_aberrations=True):
+        super().__init__()
+
+        self.setup_propagators()
+
+        @self.register_plane('science_camera', 'post_ncpa')
+        def science_camera(wf_post_ncpa):
+            return self.prop_science_camera(wf_post_ncpa)
+
+        @self.register_plane('pupil_camera', 'post_ncpa')
+        def pupil_camera(wf_post_ncpa):
+            return self.reimager_pupil_camera(wf_post_ncpa)
+
+        @self.register_plane('post_ncpa', 'pre_ncpa')
+        def post_ncpa(wf_pre_ncpa):
+            return self.ncpa(wf_pre_ncpa)
+
+        @self.register_plane('pre_ncpa', 'pre_lyot_stop')
+        def pre_ncpa(wf_pre_lyot_stop):
+            return self.lyot_stop(wf_pre_lyot_stop)
+
+        @self.register_plane('zernike_camera', 'lowfs_pupil')
+        def zernike_camera(wf_lowfs_pupil):
+            return self.reimager_zernike_camera(self.zernike_mask(wf_lowfs_pupil))
+
+        @self.register_plane('ta_camera', 'lowfs_pupil')
+        def ta_camera(wf_lowfs_pupil):
+            return self.prop_ta_camera(wf_lowfs_pupil)
+
+        @self.register_plane('lopr_camera', 'lowfs_pupil')
+        def lopr_camera(wf_lowfs_pupil):
+            return self.prop_lopr_camera(self.lopr_camera_defocus(wf_lowfs_pupil))
+
+        @self.register_plane('pre_lyot_stop', 'post_common_aberrations')
+        def pre_lyot_stop(wf_post_common_aberrations):
+            return self.magnifier_dm_to_lyot_stop(self.coro(wf_post_common_aberrations))
+
+        @self.register_plane('lowfs_pupil', 'post_common_aberrations')
+        def lowfs_pupil(wf_post_common_aberrations):
+            return self.fpm_to_lowfs(self.fpm(self.dm_to_fpm(wf_post_common_aberrations)))
+
+        @self.register_plane('pr_camera', 'post_common_aberrations')
+        def pr_camera(wf_post_common_aberrations):
+            return self.prop_pr_camera(self.defocus_pr_camera(wf_post_common_aberrations))
+
+        @self.register_plane('post_common_aberrations', 'pre_common_aberrations')
+        def post_common_aberrations(wf_pre_common_aberrations):
+            return self.common_aberrations(wf_pre_common_aberrations)
+
+        @self.register_plane('pre_common_aberrations', 'pre_boston_dms')
+        def pre_common_aberrations(wf_pre_boston_dms):
+            return self.fresnel.backward(self.dm2(self.fresnel.forward(self.dm1(wf_pre_boston_dms))))
+
+        @self.register_plane('pre_boston_dms', 'pre_apodizer')
+        def pre_boston_dms(wf_pre_apodizer):
+            return self.magnifier_apodizer_to_dm(self.apodizer(wf_pre_apodizer))
+
+        @self.register_plane('pre_apodizer', 'pre_iris_dm')
+        def pre_apodizer(wf_pre_iris_dm):
+            return self.magnifier_iris_to_apodizer(self.iris_dm(wf_pre_iris_dm))
+
+        @self.register_plane('pre_iris_dm', 'pre_pupil_mask')
+        def pre_iris_dm(wf_pre_pupil_mask):
+            return self.magnifier_pupil_to_iris(self.pupil_mask(wf_pre_pupil_mask))
+
+    def setup_propagators(self):
+        self.create_apodizer()
+        self.create_tip_tilt_mirror()
+
+        self.create_pupil_mask()
+        self.create_irisao_dm()
+        self.create_boston_dms()
+
+        self.create_beam_dump()
+        self.create_nd_filter()
+        self.create_nd_flip_mount()
+        self.create_wavefront_error()
+
+        self.create_focal_plane_mask()
+        self.create_lyot_stop()
+
+        self.create_phase_retrieval_camera()
+        self.create_zernike_camera()
+        self.create_science_camera()
+        self.create_pupil_camera()
+        self.create_target_acquisition_camera()
+        self.create_lopr_camera()
--- hicat2/simulator/optical_model.py
+++ hicat2/simulator/optical_model.py
@@ -1,15 +1,82 @@
+from collections import defaultdict
+from contextlib import contextmanager
+
 from hcipy import *
 
 class OpticalModel:
-    def __init__(self, with_aberrations=True):
-        pass
+    def __init__(self):
+        self._plane_dependencies = {}
+        self._plane_dependents = defaultdict(list)
+        self._plane_propagators = {}
 
-    def get_camera_images(self):
-        '''Get instantaneous camera images.
+        self.purge_cache()
 
-        Returns
-        -------
-        dict
-            A dictionary with hcipy Fields for each camera by name.
-        '''
-        pass
+    def purge_all(self):
+        self._cached_wavefronts = defaultdict(lambda: None)
+
+    def purge_plane(self, plane_name):
+        self._cached_wavefronts[plane_name] = None
+
+        for dep in self._plane_dependents[plane_name]:
+            self.purge_plane(dep)
+
+    @contextmanager
+    def _temperary_cache(self, new_cache=None):
+        if new_cache is None:
+            _cached_wavefronts = defaultdict(lambda: None)
+
+        old_cache = self._cache
+        self._cache = new_cache
+
+        try:
+            yield
+        finally:
+            self._cache = old_cache
+
+    def register_plane(self, plane, dependencies):
+        if isinstance(dependencies, str):
+            dependencies = [dependencies]
+
+        self._plane_dependencies[plane] = dependencies
+
+        for dep in dependencies:
+            self._plane_dependents[dep].append(plane)
+
+        def decorator(func):
+            self._plane_propagators[plane] = func
+            return func
+
+        return decorator
+
+    def propagate(self, input_plane, output_plane, wavefronts):
+        with self._temperary_cache():
+            self._cache[input_plane] = wavefronts
+
+            return getattr(self, 'wf_' + output_plane)
+
+    def set_wavefronts(self, plane, wavefronts):
+        if not isinstance(wavefronts, list):
+            wavefronts = [wavefronts]
+
+        self.purge_plane(plane)
+        self._cached_wavefronts[plane] = wavefronts
+
+    def get_wavefronts(self, plane):
+        in_cache = self._cached_wavefronts[plane]
+
+        if in_cache is not None:
+            return in_cache
+
+        prop = self._plane_propagators[plane]
+        dependencies = self._plane_dependencies
+
+        input_wavefronts = []
+        for dep in dependencies:
+            input_wavefronts.append(self.get_wavefronts(dep))
+
+        output_wavefronts = []
+        for args in zip(*input_wavefronts):
+            output_wavefronts.append(prop(*args))
+
+        self._cached_wavefronts[plane] = output_wavefronts
+        return output_wavefronts
