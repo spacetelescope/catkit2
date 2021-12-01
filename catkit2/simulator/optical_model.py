31c6aee4e323202034b115322894348334488a9e
Emiel Por <epor@stsci.edu>
2021-11-30 22:32:42 -0500

    Fix typos.

--- catkit2/simulator/optical_model.py
+++ catkit2/simulator/optical_model.py
@@ -1,8 +1,6 @@
 from collections import defaultdict
 from contextlib import contextmanager
 
-from hcipy import *
-
 def with_cached_result(getter):
     def new_getter(self):
         try:
@@ -15,6 +13,7 @@ def with_cached_result(getter):
             setattr(self, '_' + getter.__name__, res)
 
         return res
+    return new_getter
 
 class OpticalModel:
     def __init__(self):
@@ -22,7 +21,7 @@ class OpticalModel:
         self._plane_dependents = defaultdict(list)
         self._plane_propagators = {}
 
-        self.purge_cache()
+        self.purge_all()
 
     def purge_all(self):
         self._cached_wavefronts = defaultdict(lambda: None)
@@ -36,15 +35,15 @@ class OpticalModel:
     @contextmanager
     def _temperary_cache(self, new_cache=None):
         if new_cache is None:
-            _cached_wavefronts = defaultdict(lambda: None)
+            new_cache = defaultdict(lambda: None)
 
-        old_cache = self._cache
-        self._cache = new_cache
+        old_cache = self._cached_wavefronts
+        self._cached_wavefronts = new_cache
 
         try:
             yield
         finally:
-            self._cache = old_cache
+            self._cached_wavefronts = old_cache
 
     def register_plane(self, plane, dependencies):
         if isinstance(dependencies, str):
@@ -65,7 +64,12 @@ class OpticalModel:
         with self._temperary_cache():
             self.set_wavefronts(input_plane, wavefronts)
 
-            return self.get_wavefronts(output_plane)
+            res = self.get_wavefronts(output_plane)
+
+            if len(res) == 1:
+                return res[0]
+            else:
+                return res
 
     def set_wavefronts(self, plane, wavefronts):
         if not isinstance(wavefronts, list):
@@ -81,7 +85,7 @@ class OpticalModel:
             return in_cache
 
         prop = self._plane_propagators[plane]
-        dependencies = self._plane_dependencies
+        dependencies = self._plane_dependencies[plane]
 
         input_wavefronts = []
         for dep in dependencies:
