8d1431185153e79431c99f07348b59287417ac7b
Emiel Por <epor@stsci.edu>
2022-05-16 07:40:22 -0400

    Add (wip) new service class based on server.

---
+++ catkit_core/Service.cpp
@@ -0,0 +1,448 @@
+#include "Service.h"
+
+#include "TimeStamp.h"
+#include "service.pb.h"
+
+#include <chrono>
+#include <csignal>
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <zmq_addon.hpp>
+
+using namespace std;
+using namespace zmq;
+using json = nlohmann::json;
+using namespace std::string_literals;
+
+const double SAFETY_INTERVAL = 60;
+
+class Finally
+{
+public:
+	Finally(std::function<void()> func)
+		: m_Func(func)
+	{
+	}
+
+	~Finally()
+	{
+		m_Func();
+	}
+
+private:
+	std::function<void()> m_Func;
+};
+
+Service::Service(string service_id, int service_port, int testbed_port)
+	: Server(service_port), m_ServiceId(service_id),
+	m_LoggerConsole(), m_LoggerPublish(service_id, "tcp://127.0.0.1:"s + to_string(testbed_port + 1))
+{
+	m_Testbed = make_shared<TestbedProxy>("127.0.0.1", testbed_port);
+	m_Config = m_Testbed->GetConfig()["services"][service_id];
+
+	m_Testbed->UpdateServiceState(service_id, ServiceState::INITIALIZING);
+}
+
+Service::~Service()
+{
+}
+
+void Service::Run()
+{
+	if (!IsSafe())
+	{
+		LOG_CRITICAL("Testbed is unsafe. This service will not be started.");
+		return;
+	}
+
+	LOG_INFO("Opening service.");
+
+	m_Testbed.UpdateServiceState(m_ServiceId, ServiceState::OPENING);
+
+	try
+	{
+		Open();
+	}
+	catch (std::exception &e)
+	{
+		LOG_CRITICAL("Something went wrong when opening service: "s + e.what());
+		LOG_CRITICAL("Shutting down service.");
+
+		m_Testbed->UpdateServiceState(m_SerivceId, ServiceState::CRASHED);
+		return;
+	}
+
+	LOG_INFO("Service was succesfully opened.");
+
+	m_Testbed.UpdateServiceState(m_ServiceId, ServiceState::OPERATIONAL);
+
+	LOG_INFO("Starting service main function.");
+
+	{
+		std::thread main([this]()
+		{
+			Finally stop_service([this]()
+			{
+				this->ShutDown();
+			});
+
+			try
+			{
+				this->Main();
+			}
+			catch (std::exception &e)
+			{
+				LOG_CRITICAL("Something went wrong during the main function: "s + e.what());
+				LOG_CRITICAL("Shutting down service.");
+			}
+		});
+
+		std::thread safety(&Service::MonitorSafety, this);
+
+		Finally end_threads([&, this]()
+		{
+			this->ShutDown();
+
+			if (main.joinable())
+				main.join();
+
+			if (safety.joinable())
+				safety.join();
+		});
+
+		RunServer();
+	}
+
+	LOG_INFO("Service main has ended.")
+
+	m_Testbed->UpdateServiceState(m_ServiceId, ServiceState::CLOSING);
+
+	LOG_INFO("Closing service.");
+
+	try
+	{
+		Close();
+	}
+	catch (std::exception e)
+	{
+		LOG_CRITICAL("Something went wrong when closing the service: "s + e.what());
+	}
+
+	LOG_INFO("Service was closed.");
+
+	m_Testbed->UpdateServiceState(m_ServiceId, ServiceState::CLOSED);
+}
+
+void Service::MonitorSafety()
+{
+	while (!m_ShouldShutDown)
+	{
+		if (!IsSafe())
+		{
+			ShutDown();
+			return;
+		}
+
+		Sleep(SAFETY_INTERVAL);
+	}
+}
+
+bool Service::IsSafe()
+{
+	return true;
+}
+
+ProtoService::ProtoService(std::string service_name, std::string service_type, int server_port)
+	: m_ShellSocket(nullptr),
+	m_ServiceName(service_name), m_ServiceType(service_type),
+	m_Port(testbed_port), m_IsRunning(false),
+	m_HasGottenConfiguration(false), m_IsOpened(false),
+	m_LoggerConsole(), m_LoggerPublish(service_name, "tcp://localhost:"s + std::to_string(server_port + 1))
+{
+	LOG_INFO("Initializing service '"s + service_name + "'.");
+
+	// Catching Ctrl+C and similar process killers and shut down gracefully.
+	signal(SIGINT, SignalHandler);
+	signal(SIGTERM, SignalHandler);
+
+	// Set up request handlers.
+	m_RequestHandlers["get_property"] = [this](const string &data){return this->OnGetPropertyRequest(data);};
+	m_RequestHandlers["set_property"] = [this](const string &data){return this->OnSetPropertyRequest(data);};
+
+	m_RequestHandlers["execute_command"] = [this](const string &data){return this->OnExecuteCommandRequest(data);};
+
+	m_RequestHandlers["info"] = [this](const string &data){return this->OnGetInfoRequest(data);};
+
+	MakeProperty("config", [this](){return this->GetConfiguration();});
+	m_ServiceHeartbeat = MakeDataStream("heartbeat", DataType::DT_UINT64, {1}, 10);
+
+	uint64_t start_time = GetTimeStamp();
+	m_ServiceHeartbeat->SubmitData(&start_time);
+
+	LOG_INFO("Service '"s + service_name + "' has been initialized.");
+
+	m_InterfaceThread = std::thread(&ProtoService::MonitorInterface, this);
+}
+
+ProtoService::~ProtoService()
+{
+	LOG_INFO("Service '"s + m_ServiceName + "' is being destroyed.");
+
+	m_IsRunning = false;
+
+	if (m_InterfaceThread.joinable())
+		m_InterfaceThread.join();
+
+	LOG_INFO("Service '"s + m_ServiceName + "' has been destroyed.");
+}
+
+void ProtoService::Run()
+{
+	m_ShouldShutDown = false;
+
+	std::jthread service_thread = std::jthread(&ProtoService::RunService);
+
+	MonitorInterface();
+}
+
+void ProtoService::Sleep(double sleep_time_in_ms, void (*error_check)()=nullptr)
+{
+	Timer timer;
+
+	while (true)
+	{
+		double sleep_remaining = sleep_time_in_ms - timer.GetTime() * 1000;
+
+		if (sleep_remaining > 0)
+			break;
+
+		if (m_ShouldShutDown)
+			break;
+
+		if (error_check)
+			error_check();
+
+		std::this_thread::sleep_for(chrono::milliseconds(min(20, sleep_remaining)));
+	}
+}
+
+void ProtoService::ShutDown()
+{
+	m_ShouldShutDown = true;
+}
+
+void Service::Open()
+{
+}
+
+void Service::Main()
+{
+	LOG_CRITICAL("You MUST override the main() function for correct service behaviour.");
+}
+
+void Service::Close()
+{
+}
+
+std::shared_ptr<Property> Service::GetProperty(const std::string &property_name) const
+{
+	auto i = m_Properties.find(property_name);
+
+	if (i != m_Properties.end())
+		return i->second;
+	else
+		return nullptr;
+}
+
+std::shared_ptr<Command> Service::GetCommand(const std::string &command_name) const
+{
+	auto i = m_Commands.find(command_name);
+
+	if (i != m_Commands.end())
+		return i->second;
+	else
+		return nullptr;
+}
+
+std::shared_ptr<DataStream> Service::GetDataStream(const std::string &stream_name) const
+{
+	auto i = m_DataStreams.find(stream_name);
+
+	if (i != m_DataStreams.end())
+		return i->second;
+	else
+		return nullptr;
+}
+
+json Service::GetConfig() const
+{
+	return m_Config;
+}
+
+const std::string &Service::GetId() const
+{
+	return m_ServiceId;
+}
+
+std::shared_ptr<Property> Service::MakeProperty(std::string property_name, Property::Getter getter, Property::Setter setter)
+{
+	auto prop = std::make_shared<Property>(property_name, getter, setter);
+	m_Properties[property_name] = prop;
+
+	return prop;
+}
+
+std::shared_ptr<Command> Service::MakeCommand(std::string command_name, Command::CommandFunction func)
+{
+	auto cmd = std::make_shared<Command>(command_name, func);
+	m_Commands[command_name] = cmd;
+
+	return cmd;
+}
+
+std::shared_ptr<DataStream> Service::MakeDataStream(std::string stream_name, DataType type, std::vector<size_t> dimensions, size_t num_frames_in_buffer)
+{
+	auto stream = DataStream::Create(stream_name, GetServiceName(), type, dimensions, num_frames_in_buffer);
+	m_DataStreams[stream_name] = stream;
+
+	return stream;
+}
+
+std::shared_ptr<DataStream> Service::ReuseDataStream(std::string stream_name, std::string stream_id)
+{
+	auto stream = DataStream::Open(stream_id);
+	m_DataStreams[stream_name] = stream;
+
+	return stream;
+}
+
+string Service::HandleGetInfo(const string &data)
+{
+	// There's no data in the request, so don't even parse it.
+	// Create the reply protobuffer object.
+	catkit_proto::service::GetInfoReply reply;
+
+	reply.set_service_id(m_ServiceId);
+	reply.set_service_type(m_ServiceType);
+	reply.set_config(m_Config.dump());
+
+	for (auto& [key, value] : m_Properties)
+		reply.add_property_names(key);
+
+	for (auto& [key, value] : m_Commands)
+		reply.addProperty_names(key);
+
+	auto map = reply.datastream_ids();
+
+	for (auto& [key, value] : m_DataStreams)
+		map[key] = value->GetStreamId();
+
+	return reply.SerializeToString();
+}
+
+string Service::HandleGetProperty(const string &data)
+{
+	catkit_proto::service::GetPropertyRequest request;
+	request.ParseFromString(data);
+
+	std::string property_name = request.property_name();
+	auto property = GetProperty(property_name);
+
+	if (!property)
+		throw std::runtime_error("Property \""s + property_name + "\" does not exist.");
+
+	auto value = property->Get();
+
+	catkit_proto::service::GetPropertyReply reply;
+	ToProto(value, &reply.value());
+
+	return reply.SerializeToString();
+}
+
+string Service::HandleSetProperty(const string &data)
+{
+	if (!data.count("property_name"))
+		throw std::runtime_error("Request must contain a property name.");
+
+	std::string property_name = data["property_name"];
+	auto property = GetProperty(property_name);
+
+	if (!property)
+		throw std::runtime_error("Property \""s + property_name + "\" does not exist.");
+
+	if (!data.count("value"))
+		throw std::runtime_error("Request must contain a value.");
+
+	property->Set(data["value"]);
+
+	return json();
+}
+
+string Service::HandleExecuteCommand(const string &data)
+{
+	if (!data.count("command_name"))
+		throw std::runtime_error("Request must contain a command name.");
+
+	std::string command_name = data["command_name"];
+	auto command = GetCommand(command_name);
+
+	if (!command)
+		throw std::runtime_error("Command \""s + command_name + "\" does not exist.");
+
+	if (!data.count("arguments"))
+		throw std::runtime_error("Request must contain arguments.");
+
+	return command->Execute(data["arguments"]);
+}
+
+void print_usage()
+{
+	std::cout << "Usage:\n  service --name NAME --port PORT";
+}
+
+std::tuple<std::string, int> ParseServiceArgs(int argc, char *argv[])
+{
+	if (argc != 5)
+	{
+		print_usage();
+		throw std::runtime_error("Too few or too many arguments.");
+	}
+
+	std::string service_name;
+	int testbed_port;
+
+	bool name_found = false;
+	bool port_found = false;
+
+	for (size_t i = 1; i < argc; i += 2)
+	{
+		std::string arg = argv[i];
+		std::string param = argv[i + 1];
+
+		if (arg == "--name" || arg == "-n")
+		{
+			service_name = param;
+			name_found = true;
+		}
+		else if (arg == "--port" || arg == "-p")
+		{
+			testbed_port = std::stoi(param);
+			port_found = true;
+		}
+		else
+		{
+			print_usage();
+			throw std::runtime_error(std::string("Invalid argument '") + arg);
+		}
+	}
+
+	if (!(name_found && port_found))
+	{
+		print_usage();
+		throw std::runtime_error("Did not supply all arguments.");
+	}
+
+	return std::make_tuple(service_name, testbed_port);
+}
---
+++ catkit_core/Service.h
@@ -0,0 +1,75 @@
+#ifndef SERVICE_H
+#define SERVICE_H
+
+#include <vector>
+#include <string>
+#include <map>
+#include <thread>
+
+#include <zmq.hpp>
+#include <nlohmann/json.hpp>
+
+#include "Property.h"
+#include "Command.h"
+#include "DataStream.h"
+#include "LogConsole.h"
+#include "LogPublish.h"
+#include "Communication.h"
+
+class Service : public Server
+{
+public:
+	Service(std::string service_id, std::string service_type, int service_port, int testbed_port);
+	virtual ~Service();
+
+	void Run();
+
+	virtual void Open();
+	virtual void Main();
+	virtual void Close();
+
+	std::shared_ptr<Property> GetProperty(const std::string &property_name) const;
+	std::shared_ptr<Command> GetCommand(const std::string &command_name) const;
+	std::shared_ptr<DataStream> GetDataStream(const std::string &stream_name) const;
+
+	nlohmann::json GetConfig() const;
+	const std::string &GetId() const;
+
+	std::shared_ptr<Property> MakeProperty(std::string property_name, Property::Getter getter, Property::Setter setter = nullptr);
+	std::shared_ptr<Command> MakeCommand(std::string command_name, Command::CommandFunction func);
+	std::shared_ptr<DataStream> MakeDataStream(std::string stream_name, DataType type, std::vector<size_t> dimensions, size_t num_frames_in_buffer);
+	std::shared_ptr<DataStream> ReuseDataStream(std::string stream_name, std::string stream_id);
+
+private:
+	std::string HandleGetInfo(const std::string &data);
+
+	std::string HandleGetProperty(const std::string &data);
+	std::string HandleSetProperty(const std::string &data);
+
+	std::string HandleExecuteCommand(const std::string &data);
+
+	void MonitorSafety();
+	bool IsSafe();
+
+	std::shared_ptr<TestbedProxy> m_Testbed;
+
+	nlohmann::json m_Configuration;
+
+	std::shared_ptr<DataStream> m_ServiceHeartbeat;
+	std::shared_ptr<DataStream> m_ServerHeartbeat;
+	std::shared_ptr<DataStream> m_Safety;
+
+	typedef std::function<nlohmann::json(const nlohmann::json &)> MessageHandler;
+	std::map<std::string, MessageHandler> m_RequestHandlers;
+
+	std::map<std::string, std::shared_ptr<Property>> m_Properties;
+	std::map<std::string, std::shared_ptr<Command>> m_Commands;
+	std::map<std::string, std::shared_ptr<DataStream>> m_DataStreams;
+
+	LogConsole m_LoggerConsole;
+	LogPublish m_LoggerPublish;
+};
+
+std::tuple<std::string, int> ParseServiceArgs(int argc, char *argv[]);
+
+#endif // SERVICE_H
