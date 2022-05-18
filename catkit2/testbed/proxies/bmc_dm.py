a288d3ea88c51cc4d9dd4116e042a7c22906404b
sfr348 <sredmond@princeton.edu>
2022-05-18 12:16:43 -0400

    started adding useful dm poke commands

--- catkit2/testbed/proxies/bmc_dm.py
+++ catkit2/testbed/proxies/bmc_dm.py
@@ -39,3 +39,46 @@ class BmcDmProxy(ServiceProxy):
         command = self.dm_shapes_to_command(dm1_shape, dm2_shape)
 
         getattr(self, channel).submit_data(command)
+
+
+def make_poke(actuators, amplitude = quantity(700, units.nanometer), bias = False, flat_map = True, return_shortname = False, dm_num = 1):
+    """
+    Creates a 1D array DM command that can poke actuators at a given amplitude.
+    :param actuators: List of actuators, or a single actuator.
+    :param amplitude: Nanometers of amplitude
+    :param bias: Boolean flag for whether to apply a bias.
+    :param flat_map: Boolean flag for whether to apply a flat_map.
+    :param return_shortname: Boolean flag that will return a string that describes the object as the second parameter.
+    :param dm_num: 1 or 2, for DM1 or DM2.
+    :return: 1d list size 2x952
+    """
+
+    short_name = "poke"
+    total_actuators = 2*952
+    poke_array = np.zeros(total_actuators)
+
+    # Convert peak the valley from a quantity to nanometers, and get the magnitude.
+    amplitude = amplitude.to(units.meter).m
+
+    # Bias.
+    if flat_map:
+        short_name += "_flat_map"
+    if bias:
+        short_name += "_bias"
+
+    if isinstance(actuators, list):
+        for actuator in actuators:
+            poke_array[actuator] = amplitude
+            short_name += "_" + str(actuator)
+    else:
+        short_name += "_" + str(actuators)
+        poke_array[actuators] = amplitude
+
+
+    if return_shortname:
+        return poke_array, short_name
+    else:
+        return poke_array
+
+
+
