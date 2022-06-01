ce92ca4496bc6e77ee5e5842ba10ae603a8353e7
Emiel Por <epor@stsci.edu>
2022-06-01 00:17:29 -0400

    Add graph plotter for optical system.

--- catkit2/simulator/optical_model.py
+++ catkit2/simulator/optical_model.py
@@ -1,6 +1,77 @@
 from collections import defaultdict
 from contextlib import contextmanager
 
+import networkx as nx
+import pydot
+from networkx.drawing.nx_pydot import graphviz_layout
+
+import random
+
+def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
+
+    '''
+    From Joel's answer at https://stackoverflow.com/a/29597209/2966723.
+    Licensed under Creative Commons Attribution-Share Alike
+
+    If the graph is a tree this will return the positions to plot this in a
+    hierarchical layout.
+
+    G: the graph (must be a tree)
+
+    root: the root node of current branch
+    - if the tree is directed and this is not given,
+      the root will be found and used
+    - if the tree is directed and this is given, then
+      the positions will be just for the descendants of this node.
+    - if the tree is undirected and not given,
+      then a random choice will be used.
+
+    width: horizontal space allocated for this branch - avoids overlap with other branches
+
+    vert_gap: gap between levels of hierarchy
+
+    vert_loc: vertical location of root
+
+    xcenter: horizontal location of root
+    '''
+    if not nx.is_tree(G):
+        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
+
+    if root is None:
+        if isinstance(G, nx.DiGraph):
+            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
+        else:
+            root = random.choice(list(G.nodes))
+
+    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
+        '''
+        see hierarchy_pos docstring for most arguments
+
+        pos: a dict saying where all nodes go if they have been assigned
+        parent: parent of this branch. - only affects it if non-directed
+
+        '''
+
+        if pos is None:
+            pos = {root:(xcenter,vert_loc)}
+        else:
+            pos[root] = (xcenter, vert_loc)
+        children = list(G.neighbors(root))
+        if not isinstance(G, nx.DiGraph) and parent is not None:
+            children.remove(parent)
+        if len(children)!=0:
+            dx = width/len(children)
+            nextx = xcenter - width/2 - dx/2
+            for child in children:
+                nextx += dx
+                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap,
+                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
+                                    pos=pos, parent = root)
+        return pos
+
+
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
+
 def with_cached_result(getter):
     def new_getter(self):
         try:
@@ -31,7 +102,24 @@ class OpticalModel:
         self._plane_dependents = defaultdict(list)
         self._plane_propagators = {}
 
+        self.graph = nx.DiGraph()
+
         self.purge_all()
+
+    def plot_graph(self):
+        options = {
+            'node_color': 'green',
+            'node_size': 500,
+            'width': 3,
+            'arrowstyle': '-|>',
+            'arrowsize': 12,
+        }
+
+        #pos = nx.spring_layout(self.graph)
+        #pos = graphviz_layout(self.graph, prog='dot')
+        pos = hierarchy_pos(self.graph)
+
+        nx.draw_networkx(self.graph, arrows=True, pos=pos, **options)
 
     def purge_all(self):
         self._cached_wavefronts = defaultdict(lambda: None)
@@ -67,6 +155,9 @@ class OpticalModel:
         def decorator(func):
             self._plane_propagators[plane] = func
             return func
+
+        for dependency in dependencies:
+            self.graph.add_edge(dependency, plane)
 
         return decorator
 
