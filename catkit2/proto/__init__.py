94f7b90aed08ce62e1166251bbf210eef4e31ec1
Emiel Por <epor@stsci.edu>
2022-08-18 00:02:07 -0400

    Allow absolute imports from protobuf generated code.

---
+++ catkit2/proto/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+# This is HORRIBLE. Protoc produces files with absolute imports.
+# This is apparently intended behaviour. I do not understand how
+# protoc is supposed to work with multiple languages. This is
+# a simple but horrible solution to absolute imports.
+sys.path.append(os.path.dirname(__file__))
