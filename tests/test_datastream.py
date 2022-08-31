395171e2b9b89a3add614778c3825c7d3d75d303
Emiel Por <epor@stsci.edu>
2022-08-31 18:14:22 -0400

    Use pytest parametrize.

--- tests/test_datastream.py
+++ tests/test_datastream.py
@@ -1,36 +1,29 @@
 from catkit2.catkit_bindings import DataStream
 import numpy as np
+import pytest
 
-def test_data_stream():
-    dtypes = ['int8', 'uint8', 'int16', 'uint16', 'int32', 'uint32', 'int64', 'uint64', 'float32', 'float64', 'complex64', 'complex128']
-    name = 'stream_name'
-    service = 'service'
-    dimss = [[10], [10, 10], [10, 10, 10]]
+dtypes = ['int8', 'uint8', 'int16', 'uint16', 'int32', 'uint32', 'int64', 'uint64', 'float32', 'float64', 'complex64', 'complex128']
+shapes = [[10], [10, 10], [10, 10, 10], [10, 10, 10, 10]]
 
-    for dims in dimss:
-        for dtype in dtypes:
-            created_stream = DataStream.create(name, 'service', dtype, dims, 20)
+@pytest.mark.parametrize("shape", shapes)
+@pytest.mark.parametrize("dtype", dtypes)
+def test_data_stream(shape, dtype):
+    created_stream = DataStream.create('stream_name', 'service', dtype, shape, 20)
 
-            assert created_stream.dtype == dtype
-            assert np.allclose(created_stream.shape, dims)
+    assert created_stream.dtype == dtype
+    assert np.allclose(created_stream.shape, shape)
 
-            opened_stream = DataStream.open(created_stream.stream_id)
+    opened_stream = DataStream.open(created_stream.stream_id)
 
-            assert opened_stream.dtype == dtype
-            assert np.allclose(opened_stream.shape, dims)
+    assert opened_stream.dtype == dtype
+    assert np.allclose(opened_stream.shape, shape)
 
-            data = np.random.randn(*dims).astype(dtype)
-            created_stream.submit_data(data)
+    data = np.random.randn(*shape).astype(dtype)
+    created_stream.submit_data(data)
 
-            frame = opened_stream.get_latest_frame()
+    frame = opened_stream.get_latest_frame()
 
-            assert frame.id == 0
+    assert frame.id == 0
 
-            assert np.allclose(frame.data, data)
-            assert np.allclose(opened_stream.get(), data)
-
-            # Since we are creating a lot of data streams with the same name, we need to
-            # ensure that the shared memory is closed before trying to reopen the same
-            # part of memory. The order doesn't matter.
-            del created_stream
-            del opened_stream
+    assert np.allclose(frame.data, data)
+    assert np.allclose(opened_stream.get(), data)
