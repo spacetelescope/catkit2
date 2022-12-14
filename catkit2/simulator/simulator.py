d754c194c301305c1c9489a124572adefe9a3c5f
Emiel Por <epor@stsci.edu>
2022-12-13 22:02:33 -0500

    Add docstrings.

--- catkit2/simulator/simulator.py
+++ catkit2/simulator/simulator.py
@@ -13,6 +13,35 @@ class Callback(namedtuple('Callback', ['time', 'id', 'func'])):
 
 class Simulator(Service):
     def __init__(self, service_type, max_time_factor=1):
+        '''Base class for simultator services.
+
+        This simulator uses a callback system to schedule events on a simulated
+        timeline. Methods to be implemented by the derived class integrate with
+        a system of simulated services to automate DM actuations, stage movements,
+        flip mounts, filter wheels, power strips, and, of course, camera acquisitions.
+        Cameras use a reset (which indicates the start of an exposure) and a readout
+        (which indicates the end of an exposure). Camera images are integrated over
+        time while the other events are handled.
+
+        Parameters
+        ----------
+        service_type : string
+            The service type of the simulator. This will be passed onto the Service
+            init().
+        max_time_factor : scalar
+            The maximum speed of the simulated time relative to wall clock time. If
+            images are generated too fast, the simulator will slow down to this fraction
+            of wall clock time. This enables other services to respond in time and react
+            to camera images in the same way as they would do when ran on hardware.
+            Default value: 1.
+
+        Attributes
+        ----------
+        model : OpticalModel
+            The optical model that the simulator is basing its images on.
+        time : DataStream
+            The current simulator time.
+        '''
         super().__init__(service_type)
 
         self.max_time_factor = max_time_factor
@@ -82,6 +111,21 @@ class Simulator(Service):
             callback.func(callback.time)
 
     def add_callback(self, callback_func, t=None):
+        '''Add a callback to the callback list.
+
+        Parameters
+        ----------
+        callback_func : function
+            The function to execute when the time `t` has been reached by the simulator.
+        t : scalar or None
+            The simulated time at which to run the function. A value of None (the default)
+            indicates that the callback should be executed at the current simulated time.
+
+        Returns
+        -------
+        Callback
+            The created callback.
+        '''
         with self.lock:
             if t is None:
                 t = self.time.get()[0]
@@ -94,6 +138,16 @@ class Simulator(Service):
         return callback
 
     def remove_callback(self, callback):
+        '''Remove a callback from the list of callbacks.
+
+        This allows the user to cancel a previously added callback. If the
+        callback has already been executed, this function is a no-op.
+
+        Parameters
+        ----------
+        callback : Callback
+            The callback which to remove from the list of callbacks.
+        '''
         with self.lock:
             if callback not in self.callbacks:
                 return
@@ -104,13 +158,52 @@ class Simulator(Service):
 
             heapq.heapify(self.callbacks)
 
-    def set_breakpoint(self, at_time):
-        raise NotImplementedError
+    def set_breakpoint(self, t_until=None):
+        '''Set a breakpoint for the simulator.
+
+        The simulator will pause at this breakpoint until it is released. Currently,
+        this functionality is not implemented.
+
+        Parameters
+        ----------
+        t_until : scalar
+            The simulated time of the breakpoint.
+
+        Returns
+        -------
+        int
+            A token for the breakpoint.
+        '''
+        raise NotImplementedError()
 
     def release_breakpoint(self, breakpoint_token):
-        raise NotImplementedError
+        '''Release a previously set breakpoint.
+
+        This will let the simulator continue past the breakpoint. When the
+        breakpoint has not been reached yet, this will have no influence.
+
+        Parameters
+        ----------
+        breakpoint_token : int
+            A token given out by `set_breakpoint()`.
+        '''
+        raise NotImplementedError()
 
     def start_camera_acquisition(self, camera_name, integration_time, frame_interval):
+        '''Start acquisition on a camera.
+
+        The camera will take images every `frame_interval` seconds with an integration time
+        of `integration_time`.
+
+        Parameters
+        ----------
+        camera_name : string
+            The name of the camera.
+        integration_time : scalar
+            The integration time of the camera.
+        frame_interval : scalar
+            The time interval between neighboring frames.
+        '''
         def callback(t):
             self.log.info(f'Start camera acquisition on {camera_name} with {integration_time} and {frame_interval}.')
 
@@ -132,6 +225,16 @@ class Simulator(Service):
         self.add_callback(callback)
 
     def make_camera_reset_callback(self, camera_name):
+        '''Schedule a new reset callback for a camera.
+
+        This function will usually be called a certain time after a camera has been read out.
+        This marks the start of an exposure for this camera.
+
+        Parameters
+        ----------
+        camera_name : string
+            The name of the camera.
+        '''
         def callback(t):
             if camera_name not in self.integrating_cameras:
                 # Camera has stopped.
@@ -155,6 +258,20 @@ class Simulator(Service):
         return callback
 
     def make_camera_readout_callback(self, camera_name, integration_time, frame_interval):
+        '''Schedule a new readout callback for a camera.
+
+        This function will usually be called a certain time after a camera has been reset.
+        This marks the end of an exposure for this camera.
+
+        Parameters
+        ----------
+        camera_name : string
+            The name of the camera.
+        integration_time : scalar
+            The integration time for this camera.
+        frame_interval : scalar
+            The time interval between neighboring frames.
+        '''
         def callback(t):
             if camera_name not in self.camera_integrated_power:
                 return
@@ -169,7 +286,7 @@ class Simulator(Service):
 
             if camera_name in self.integrating_cameras:
                 # We are still integrating, so schedule reset.
-                next_reset_time = self.time.get()[0] - integration_time + frame_interval
+                next_reset_time = t - integration_time + frame_interval
                 next_reset_callback = self.make_camera_reset_callback(camera_name)
 
                 callback = self.add_callback(next_reset_callback, next_reset_time)
@@ -179,6 +296,16 @@ class Simulator(Service):
         return callback
 
     def end_camera_acquisition(self, camera_name):
+        '''End the acquisition loop on a camera.
+
+        The current exposure will be finished, but afterwards, no more exposures
+        will be performed on this camera (until the acquisition is started again).
+
+        Parameters
+        ----------
+        camera_name : string
+            The name of the camera.
+        '''
         def callback(t):
             self.log.info(f'End camera acquisition on {camera_name}.')
             if camera_name in self.integrating_cameras:
@@ -186,20 +313,98 @@ class Simulator(Service):
 
         self.add_callback(callback)
 
-    def camera_readout(self, camera_name, power):
+    def camera_readout(self, camera_name, integrated_power):
+        '''Read out a camera.
+
+        This function should be overriden by the child class. It is responsible
+        for handling the image and submitting it to the right service.
+
+        Parameters
+        ----------
+        camera_name : string
+            The name of the camera.
+        integrated_power : hcipy.Field
+            The integrated power for this camera, ie. integration time times average
+            incident power on each pixel.
+        '''
         pass
 
     def actuate_dm(self, dm_name, new_actuators):
+        '''Actuate a DM.
+
+        This function should be overriden by the child class. It is responsible
+        for setting the actuators on the DM in its model.
+
+        Parameters
+        ----------
+        dm_name : string
+            The name of the DM.
+        new_actuators : ndarray
+            The new actuators for this DM.
+        '''
         pass
 
     def move_stage(self, stage_name, old_stage_position, new_stage_position):
+        '''Move a stage.
+
+        This function should be overriden by the child class. It is responsible
+        for setting the stage position in its model.
+
+        Having access to both the old and new stage position allows for the child
+        class to implement movements of a stage over simulated time.
+
+        Parameters
+        ----------
+        stage_name : string
+            The name of the stage.
+        old_stage_position : scalar
+            The current position of this stage axis.
+        new_stage_position : scalar
+            The commanded position of this stage axis.
+        '''
         pass
 
     def move_filter(self, filter_wheel_name, new_filter_position):
+        '''Move a filter.
+
+        This function should be overriden by the child class. It is responsible
+        for setting the filter in its model.
+
+        Parameters
+        ----------
+        filter_wheel_name : string
+            The name of the filter wheel.
+        new_filter_position : scalar or child-class defined
+            The new filter position for this filter wheel.
+        '''
         pass
 
     def move_flip_mount(self, flip_mount_name, new_flip_mount_position):
+        '''Move a flip mount.
+
+        This function should be overriden by the child class. It is responsible
+        for setting the flip mount in its model.
+
+        Parameters
+        ----------
+        flip_mount_name : string
+            The name of the flip mount.
+        new_flip_mount_position : scalar
+            The new position of this flip mount.
+        '''
         pass
 
     def switch_power(self, outlet_name, powered):
+        '''Switch power on an outlet on or off.
+
+        This function should be overriden by the child class. It is responsible
+        for setting whatever effect this outlet has on the model.
+
+        Parameters
+        ----------
+        outlet_name : string
+            The name of the outlet.
+        powered : boolean
+            Whether to turn the power on (True) or off (False).
+        '''
         pass
