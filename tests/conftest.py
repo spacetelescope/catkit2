829a07b56761689ade856d737e5fdf2a4c566d73
Emiel Por <epor@stsci.edu>
2023-04-18 21:34:45 -0400

    Create fixtures for testbed and dummy service.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,35 @@ def unused_port():
             return sock.getsockname()[1]
 
     return get
+
+def run_testbed(port):
+    is_simulated = False
+
+    config_path = pathlib.Path(os.path.join(os.path.dirname(__file__), 'config'))
+    config_files = config_path.resolve().glob('*.yml')
+    config = read_config_files(config_files)
+
+    testbed = Testbed(port, is_simulated, config)
+    testbed.run()
+
+@pytest.fixture(scope='session')
+def testbed(unused_port):
+    port = unused_port()
+
+    process = multiprocessing.Process(target=run_testbed, args=(port,))
+    process.start()
+
+    testbed = TestbedProxy('127.0.0.1', port)
+
+    yield testbed
+
+    testbed.shut_down()
+    process.join()
+
+@pytest.fixture(scope='session')
+def test_service(testbed):
+    testbed.start_service('test_service')
+
+    yield testbed.test_service
+
+    testbed.stop_service('test_service')
