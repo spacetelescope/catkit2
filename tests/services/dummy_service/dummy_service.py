cb7d8d928e970f430feecfbe63385b95b88ed321
Emiel Por <epor@stsci.edu>
2023-04-18 21:34:25 -0400

    Add dummy testbed and service.

---
+++ tests/services/dummy_service/dummy_service.py
@@ -0,0 +1,37 @@
+from catkit2 import Service
+
+class DummyService(Service):
+    def __init__(self):
+        super().__init__('dummy_service')
+
+        self.readonly_property = 0
+        self.readwrite_property = 1
+
+    def open(self):
+        self.make_property('readonly_property', self.get_readonly)
+        self.make_property('readwrite_property', self.get_readwrite, self.set_readwrite)
+
+        self.make_command('add', self.add)
+
+    def main(self):
+        while not self.should_shut_down:
+            self.sleep(1)
+
+    def close(self):
+        pass
+
+    def get_readonly(self):
+        return self.readonly_property
+
+    def get_readwrite(self):
+        return self.readwrite_property
+
+    def set_readwrite(self, value):
+        self.readwrite_property = value
+
+    def add(self, a, b):
+        return a + b
+
+if __name__ == '__main__':
+    service = DummyService()
+    service.run()
