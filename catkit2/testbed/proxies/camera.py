153ecd5d2651f4651c3d9b9b90c6307829f818fe
Sarah Steiger <ssteiger@stsci.edu>
2023-10-03 09:24:33 -0400

    add take_e_field_exposures function to camera.py and give the  e_field_camera a camera interface to access it

--- catkit2/testbed/proxies/camera.py
+++ catkit2/testbed/proxies/camera.py
@@ -37,6 +37,37 @@ class CameraProxy(ServiceProxy):
             if not was_acquiring:
                 self.end_acquisition()
 
+    def take_e_field_exposures(self, num_exposures, weights):
+        was_acquiring = self.is_acquiring.get()[0]
+        if not was_acquiring:
+            self.start_acquisition()
+
+        first_frame_id = self.e_field_images.newest_available_frame_id
+
+        if was_acquiring:
+            # Ignore first two frames to ensure the frames were taken _after_ the
+            # call to this function. This is not necessary if the acquisition just
+            # started.
+            first_frame_id += 2
+
+        try:
+            i = 0
+            num_exposures_remaining = num_exposures
+
+            while num_exposures_remaining >= 1:
+                try:
+                    frame = self.e_field_images.get_frame(first_frame_id + i, 100000)
+                except RuntimeError:
+                    # The frame wasn't available anymore because we were waiting too long.
+                    continue
+                finally:
+                    i += 1
+
+                yield frame.data.copy() * weights
+                num_exposures_remaining -= 1
+        finally:
+            self.end_acquisition()
+
     def take_exposures(self, *args, **kwargs):
         warnings.warn('Please use camera.take_raw_exposures() instead.', DeprecationWarning)
         yield from self.take_raw_exposures(*args, **kwargs)
