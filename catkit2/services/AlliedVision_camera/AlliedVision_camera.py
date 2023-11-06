d6cecbd55e8a450b868ee714b644f39b8fb4eb6c
Iva Laginja <iva.laginja@obspm.fr>
2023-11-06 14:52:23 +0100

    Use ExitStack for a single with statement outside of the class

--- catkit2/services/AlliedVision_camera/AlliedVision_camera.py
+++ catkit2/services/AlliedVision_camera/AlliedVision_camera.py
@@ -1,5 +1,6 @@
 import threading
 import numpy as np
+import contextlib
 
 from catkit2.testbed.service import Service
 
@@ -9,82 +10,83 @@ from vimba import FrameStatus, PixelFormat, Vimba
 class AlliedVisionCamera(Service):
     NUM_FRAMES = 20
 
-    def __init__(self):
+    def __init__(self, exit_stack):
         super().__init__('allied_vision_camera')
+
+        self.exit_stack = exit_stack
 
         self.should_be_acquiring = threading.Event()
         self.should_be_acquiring.set()
 
     def open(self):
-        with Vimba.get_instance() as vimba:
-            if len(vimba.get_all_cameras()) == 0:
-                raise RuntimeError("Not a single Allied Vision camera is connected.")
+        self.vimba = vimba.get_instance()
+        self.exit_stack.enter(self.vimba)
 
-            with vimba.get_all_cameras()[0] as cam:
-                # Set device values from config file (set width and height before offsets)
-                offset_x = self.config.get('offset_x', 0)
-                offset_y = self.config.get('offset_y', 0)
+        self.cam = self.vimba.get_all_cameras()[0]
+        self.exit_stack.enter(self.cam)
 
-                self.width = self.config.get('width', self.sensor_width - offset_x)
-                self.height = self.config.get('height', self.sensor_height - offset_y)
-                self.offset_x = offset_x
-                self.offset_y = offset_y
+        # Set device values from config file (set width and height before offsets)
+        offset_x = self.config.get('offset_x', 0)
+        offset_y = self.config.get('offset_y', 0)
 
-                self.gain = self.config.get('gain', 0)
-                self.exposure_time_step_size = self.config.get('exposure_time_step_size', 1)
-                self.exposure_time_offset_correction = self.config.get('exposure_time_offset_correction', 0)
-                self.exposure_time_base_step = self.config.get('exposure_time_base_step', 1)
-                self.exposure_time = self.config.get('exposure_time', 1000)
+        self.width = self.config.get('width', self.sensor_width - offset_x)
+        self.height = self.config.get('height', self.sensor_height - offset_y)
+        self.offset_x = offset_x
+        self.offset_y = offset_y
 
-                # Create datastreams
-                # Use the full sensor size here to always allocate enough shared memory.
-                self.images = self.make_data_stream('images', 'float32', [self.sensor_height, self.sensor_width], self.NUM_FRAMES)
-                print(self.sensor_height)
-                print(self.sensor_width)
-                print(self.height)
-                print(self.width)
-                self.temperature = self.make_data_stream('temperature', 'float64', [1], self.NUM_FRAMES)
+        self.gain = self.config.get('gain', 0)
+        self.exposure_time_step_size = self.config.get('exposure_time_step_size', 1)
+        self.exposure_time_offset_correction = self.config.get('exposure_time_offset_correction', 0)
+        self.exposure_time_base_step = self.config.get('exposure_time_base_step', 1)
+        self.exposure_time = self.config.get('exposure_time', 1000)
 
-                self.is_acquiring = self.make_data_stream('is_acquiring', 'int8', [1], self.NUM_FRAMES)
-                self.is_acquiring.submit_data(np.array([0], dtype='int8'))
+        # Create datastreams
+        # Use the full sensor size here to always allocate enough shared memory.
+        self.images = self.make_data_stream('images', 'float32', [self.sensor_height, self.sensor_width], self.NUM_FRAMES)
+        print(self.sensor_height)
+        print(self.sensor_width)
+        print(self.height)
+        print(self.width)
+        self.temperature = self.make_data_stream('temperature', 'float64', [1], self.NUM_FRAMES)
 
-                # Create properties
-                def make_property_helper(name, read_only=False, requires_stopped_acquisition=False):
-                    if read_only:
-                        self.make_property(name, lambda: getattr(self, name))
-                    else:
-                        if requires_stopped_acquisition:
-                            def setter(val):
-                                with StoppedAcquisition(self):
-                                    setattr(self, name, val)
-                        else:
-                            def setter(val):
-                                setattr(self, name, val)
+        self.is_acquiring = self.make_data_stream('is_acquiring', 'int8', [1], self.NUM_FRAMES)
+        self.is_acquiring.submit_data(np.array([0], dtype='int8'))
 
-                        self.make_property(name, lambda: getattr(self, name), setter)
+        # Create properties
+        def make_property_helper(name, read_only=False, requires_stopped_acquisition=False):
+            if read_only:
+                self.make_property(name, lambda: getattr(self, name))
+            else:
+                if requires_stopped_acquisition:
+                    def setter(val):
+                        with StoppedAcquisition(self):
+                            setattr(self, name, val)
+                else:
+                    def setter(val):
+                        setattr(self, name, val)
 
-                make_property_helper('exposure_time')
-                make_property_helper('gain')
-                make_property_helper('brightness')
+                self.make_property(name, lambda: getattr(self, name), setter)
 
-                make_property_helper('width', requires_stopped_acquisition=True)
-                make_property_helper('height', requires_stopped_acquisition=True)
-                make_property_helper('offset_x')
-                make_property_helper('offset_y')
+        make_property_helper('exposure_time')
+        make_property_helper('gain')
+        make_property_helper('brightness')
 
-                make_property_helper('sensor_width', read_only=True)
-                make_property_helper('sensor_height', read_only=True)
+        make_property_helper('width', requires_stopped_acquisition=True)
+        make_property_helper('height', requires_stopped_acquisition=True)
+        make_property_helper('offset_x')
+        make_property_helper('offset_y')
 
-                make_property_helper('device_name', read_only=True)
+        make_property_helper('sensor_width', read_only=True)
+        make_property_helper('sensor_height', read_only=True)
 
-                self.make_command('start_acquisition', self.start_acquisition)
-                self.make_command('end_acquisition', self.end_acquisition)
+        make_property_helper('device_name', read_only=True)
+
+        self.make_command('start_acquisition', self.start_acquisition)
+        self.make_command('end_acquisition', self.end_acquisition)
 
     def main(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                cam.start_streaming(handler=self.acquisition_loop, buffer_count=self.NUM_FRAMES)
-                cam.stop_streaming()
+        self.cam.start_streaming(handler=self.acquisition_loop, buffer_count=self.NUM_FRAMES)
+        self.cam.stop_streaming()
 
     def close(self):
         # close cam AV
@@ -116,101 +118,70 @@ class AlliedVisionCamera(Service):
 
     @property
     def exposure_time(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.ExposureTime.get()
+        return self.cam.ExposureTime.get()
 
     @exposure_time.setter
     def exposure_time(self, exposure_time):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                cam.ExposureTime.set(exposure_time)
+        self.cam.ExposureTime.set(exposure_time)
 
     @property
     def gain(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.Gain.get()
+        return self.cam.Gain.get()
 
     @gain.setter
     def gain(self, gain):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                cam.Gain.set(gain)
+        self.cam.Gain.set(gain)
 
     @property
     def brightness(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.Brightness.get()
+        return self.cam.Brightness.get()
 
     @brightness.setter
     def brightness(self, brightness):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                cam.Brightness.set(brightness)
+        self.cam.Brightness.set(brightness)
 
     @property
     def sensor_width(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.SensorWidth.get()
+        return self.cam.SensorWidth.get()
 
     @property
     def sensor_height(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.SensorHeight.get()
+        return self.cam.SensorHeight.get()
 
     @property
     def width(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.Width.get()
+        return self.cam.Width.get()
 
     @width.setter
     def width(self, width):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                cam.Width.set(width)
+        self.cam.Width.set(width)
 
     @property
     def height(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.Height.get()
+        return self.cam.Height.get()
 
     @height.setter
     def height(self, height):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                cam.Height.set(height)
+        self.cam.Height.set(height)
 
     @property
     def offset_x(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.OffsetX.get()
+        return self.cam.OffsetX.get()
 
     @offset_x.setter
     def offset_x(self, offset_x):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                cam.OffsetX.set(offset_x)
+        self.cam.OffsetX.set(offset_x)
 
     @property
     def offset_y(self):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                return cam.OffsetY.get()
+        return self.cam.OffsetY.get()
 
     @offset_y.setter
     def offset_y(self, offset_y):
-        with Vimba.get_instance() as vimba:
-            with vimba.get_all_cameras()[0] as cam:
-                cam.OffsetY.set(offset_y)
+        self.cam.OffsetY.set(offset_y)
 
 
 if __name__ == '__main__':
-    service = AlliedVisionCamera()
-    service.run()
+    with contextlib.ExitStack() as exit_stack:
+        service = AlliedVisionCamera(exit_stack)
+        service.run()
