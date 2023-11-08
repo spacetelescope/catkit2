213432df8837e5a4f89a53245381d4c9ae186079
Iva Laginja <iva.laginja@obspm.fr>
2023-11-08 20:47:08 +0100

    Add frame handler

--- catkit2/services/AlliedVision_camera/AlliedVision_camera.py
+++ catkit2/services/AlliedVision_camera/AlliedVision_camera.py
@@ -92,9 +92,15 @@ class AlliedVisionCamera(Service):
         if not has_correct_parameters:
             self.images.update_parameters('float32', [self.sensor_height, self.sensor_width], self.NUM_FRAMES)
 
+        def frame_handler(frame):
+            self.cam.queue_frame(frame)
+
+        # Start acquisition.
+        self.cam.start_streaming(handler=frame_handler, buffer_count=self.NUM_FRAMES)
+        self.is_acquiring.submit_data(np.array([1], dtype='int8'))
+
         try:
             while self.should_be_acquiring.is_set() and not self.should_shut_down:
-                self.cam.start_streaming(handler=frame_handler, buffer_count=self.NUM_FRAMES)
                 if frame.get_status() == self.vimba.FrameStatus.Complete:
                     frame.convert_pixel_format(self.vimba.PixelFormat.Mono8)  # TODO change
                     print(frame.as_numpy_ndarray().astype('float32').shape)
