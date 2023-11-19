26826d8d5456dc775f1dedac1156bbd513a22ddd
Iva Laginja <iva.laginja@obspm.fr>
2023-11-19 23:33:15 +0100

    Add simulated service for Thorlabs laser diode

---
+++ catkit2/services/thorlabs_cld101x_sim/thorlabs_cld101x_sim.py
@@ -0,0 +1,47 @@
+from catkit2.testbed.service import Service
+
+import pyvisa
+import numpy as np
+
+
+class ThorlabsCLD101XSim(Service):
+    def __init__(self):
+        super().__init__('thorlabs_cld101x_sim')
+
+        self.current_setpoint = self.make_data_stream('current_setpoint', 'float64', [1], 20)
+        self.current_percent = self.make_data_stream('current_percent', 'float64', [1], 20)
+
+    def open(self):
+        self.make_command('set_current_setpoint', self.set_current_setpoint)
+        self.max_current = 25  # in Ampere  # TODO: Get this from config?
+
+    def main(self):
+        while not self.should_shut_down:
+            self.sleep(1)
+
+    def close(self):
+        pass
+
+    def set_current_setpoint(self, current_percent):
+        """
+        Set the current setpoint of the laser, controlled as percent of its max current setpoint.
+
+        Parameters
+        ----------
+        current_percent : int
+            Limited to range 0-100, in percent of the max current setpoint in Ampere.
+        """
+        if current_percent < 0 or current_percent > 100:
+            raise ValueError("Current_percent must be between 0 and 100.")
+
+        current_setpoint = current_percent / 100 * self.max_current
+
+        self.testbed.simulator.set_source_power(source_name=test, power=current_percent)
+
+        self.current_setpoint.submit_data(np.array([current_setpoint]))
+        self.current_percent.submit_data(np.array([current_percent]))
+
+
+if __name__ == '__main__':
+    service = ThorlabsCLD101XSim()
+    service.run()
