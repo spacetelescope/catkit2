efb3151d731b9a89664090177a2a73e596f07ead
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2024-03-07 12:35:06 +0100

    Update proxy imports in __init__.py

--- catkit2/testbed/proxies/__init__.py
+++ catkit2/testbed/proxies/__init__.py
@@ -1,4 +1,6 @@
-__all__ = [
+__all__ = []
+
+[
     'CameraProxy',
     'NewportXpsQ8Proxy',
     'FlipMountProxy',
@@ -9,11 +11,51 @@ __all__ = [
     'WebPowerSwitchProxy'
 ]
 
-from .bmc_dm import *
-from .camera import *
-from .newport_xps import *
-from .flip_mount import *
-from .newport_picomotor import *
-from .ni_daq import *
-from .nkt_superk import *
-from .web_power_switch import *
+try:
+    from .bmc_dm import *
+    __all__.append('BmcDmProxy')
+except ImportError:
+    print('BmcDmProxy not imported')
+
+try:
+    from .camera import *
+    __all__.append('CameraProxy')
+except ImportError:
+    print('CameraProxy not imported')
+
+try:
+    from .newport_xps import *
+    __all__.append('NewportXpsQ8Proxy')
+except ImportError:
+    print('NewportXpsQ8Proxy not imported')
+
+
+try:
+    from .flip_mount import *
+    __all__.append('FlipMountProxy')
+except ImportError:
+    print('FlipMountProxy not imported')
+
+try:
+    from .newport_picomotor import *
+    __all__.append('NewportPicomotorProxy')
+except ImportError:
+    print('NewportPicomotorProxy not imported')
+
+try:
+    from .ni_daq import *
+    __all__.append('NiDaqProxy')
+except ImportError:
+    print('NiDaqProxy not imported')
+
+try:
+    from .nkt_superk import *
+    __all__.append('NktSuperkProxy')
+except ImportError:
+    print('NktSuperkProxy not imported')
+
+try:
+    from .web_power_switch import *
+    __all__.append('WebPowerSwitchProxy')
+except ImportError:
+    print('WebPowerSwitchProxy not imported')
