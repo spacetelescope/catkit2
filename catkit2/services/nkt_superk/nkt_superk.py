f5848c25f00aef822f2a2a1c5cf82e737f130a95
Remi Soummer <soummer@stsci.edu>
2024-04-05 15:43:44 -0400

    Removed the direct import to the copy pasted DLL python file, and replaced by pointing to the file in the SDK installed location using the environment variable

--- catkit2/services/nkt_superk/nkt_superk.py
+++ catkit2/services/nkt_superk/nkt_superk.py
@@ -5,7 +5,17 @@ from concurrent.futures import ThreadPoolExecutor
 import threading
 from enum import Enum
 
-from NKTP_DLL import *
+
+try:
+    sdk_path = os.path.join(os.environ.get('NKTP_SDK_PATH'), 'Examples', 'DLL_Example_Python')
+    if sdk_path is not None:
+        sys.path.append(sdk_path)
+
+    from NKTP_DLL import *
+except ImportError:
+    print('To use NKT SDK, you need to install the SDK and check the NKTP_SDK_PATH environment variable.')
+    raise
+
 
 # SuperK EVO registers.
 class Evo(Enum):
