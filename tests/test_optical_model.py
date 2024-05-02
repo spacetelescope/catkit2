50e310d776fc4366f7efd9ba8e7156d0660d3b11
Iva Laginja <iva.laginja@obspm.fr>
2024-05-02 15:08:07 +0200

    Remove data stream from optical model test

--- tests/test_optical_model.py
+++ tests/test_optical_model.py
@@ -3,7 +3,6 @@ from catkit2.simulator import SimpleOpticalModel
 
 import hcipy
 import numpy as np
-import time
 
 
 def test_optical_model():
@@ -12,21 +11,11 @@ def test_optical_model():
 
     wf_in = hcipy.Wavefront(pupil_grid.ones())
     model.set_wavefronts('pre_pupil', wf_in)
-    
-    test_wf = model.get_wavefronts('science_camera')[0]
-    size1 = test_wf.intensity.shaped.shape[0]
-    size2 = test_wf.intensity.shaped.shape[1]
-
-    stream = DataStream.create('images', 'mod', 'float64', [size1, size2], 20)
-    # print(stream.stream_id)
-    time.sleep(0.01)
 
     for i in range(5):
         model.atmosphere.t += 0.01
         model.purge_plane('pre_coro')
 
         wf_out = model.get_wavefronts('science_camera')[0]
-
-        stream.submit_data(wf_out.intensity.shaped)
 
         assert np.sum(wf_out.intensity) > 0
