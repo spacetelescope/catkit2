332ddfb3984ed9c1bfe65766152dc0fb91450b1a
THD2-team <thd2team@gmail.com>
2024-05-09 13:56:12 +0200

    Clean up and make work

--- catkit2/services/thorlabs_cube_motor_kinesis/thorlabs_cube_motor_kinesis.py
+++ catkit2/services/thorlabs_cube_motor_kinesis/thorlabs_cube_motor_kinesis.py
@@ -1,26 +1,13 @@
-from ctypes import c_byte, c_char, c_ulong, c_int, c_short, c_ushort, c_double
-from ctypes import cdll, CDLL, Structure, byref, create_string_buffer
+from ctypes import c_ulong, c_int, c_ushort, c_double
+from ctypes import cdll, CDLL, byref, create_string_buffer
 
 import os
 import sys
+import time
 
 import numpy as np
 
 from catkit2.testbed.service import Service
-
-
-class TLI_HardwareInformation(Structure):
-    _fields_ = [
-        ('serialNumber', c_ulong),
-        ('modelNumber', c_char * 8),
-        ('type', c_ushort),
-        ('firmwareVersion', c_ulong),
-        ('notes', c_char * 48),
-        ('deviceDependantData', c_byte * 12),
-        ('hardwareVersion', c_ushort),
-        ('modificationState', c_ushort),
-        ('numChannels', c_short),
-    ]
 
 
 class ThorlabsCubeMotorKinesis(Service):
@@ -61,6 +48,9 @@ class ThorlabsCubeMotorKinesis(Service):
         self.min_position_config = None
         self.max_position_config = None
 
+        self.command = None
+        self.current_position = None
+
     def open(self):
         """
         Open connection to the motor.
@@ -72,8 +62,10 @@ class ThorlabsCubeMotorKinesis(Service):
             serial_number_list_size = 35  # 4 * 8 (SN size) + 3 for the commas between the serial numbers
             serial_number_list = create_string_buffer(serial_number_list_size)
             self.lib.TLI_GetDeviceListByTypeExt(serial_number_list, serial_number_list_size, self.motor_type)
-            if serial_number_list not in self.serial_number:
-                raise ValueError(f"Device with serial number {self.serial_number} not found.")
+            self.log.debug("Serial number: %s", self.serial_number)
+            self.log.debug("Serial number list: %s", serial_number_list.value)
+            if self.serial_number not in serial_number_list.value:
+                raise ValueError(f"Device with serial number {self.serial_number} not found in list of serial numbers: {serial_number_list.value}")
 
         # Open the device
         self.lib.CC_Open(self.serial_number)
@@ -90,16 +82,35 @@ class ThorlabsCubeMotorKinesis(Service):
         # Apply these values to the device
         self.lib.CC_SetMotorParamsExt(self.serial_number, steps_per_rev, gear_box_ratio, pitch_mm)
 
-        # Retrieve the min, max, unit and model from the connected device.
-        real_unit = c_double()
-        self.lib.CC_GetRealValueFromDeviceUnit(self.serial_number,
-                                               self.lib.CC_GetStageAxisMinPos(self.serial_number),
-                                               byref(real_unit), 0)
-        self.min_position_device = real_unit.value
-        self.lib.CC_GetRealValueFromDeviceUnit(self.serial_number,
-                                               self.lib.CC_GetStageAxisMaxPos(self.serial_number),
-                                               byref(real_unit), 0)
-        self.max_position_device = real_unit.value
+        # Read min, max, unit and model from service configuration.
+        self.min_position_config = self.config['min_position']
+        self.max_position_config = self.config['max_position']
+        unit_config = self.config['unit']
+
+        # Set the motor travel limits.
+        min_position = c_double(self.min_position_config)
+        max_position = c_double(self.max_position_config)
+        self.lib.CC_SetMotorTravelLimits(self.serial_number, min_position, max_position)
+
+        # Get the motor travel limits.
+        min_position = c_double(-1)
+        max_position = c_double(-1)
+        self.lib.CC_GetMotorTravelLimits(self.serial_number, byref(min_position), byref(max_position))
+        self.min_position_device = min_position.value
+        self.max_position_device = max_position.value
+
+        # # Retrieve the min, max, unit and model from the connected device.
+        # real_unit = c_double()
+        # min_pos = self.lib.CC_GetStageAxisMinPos(self.serial_number)
+        # self.lib.CC_GetRealValueFromDeviceUnit(self.serial_number,
+        #                                        min_pos,
+        #                                        byref(real_unit), 0)
+        # self.min_position_device = real_unit.value
+        # max_pos = self.lib.CC_GetStageAxisMaxPos(self.serial_number)
+        # self.lib.CC_GetRealValueFromDeviceUnit(self.serial_number,
+        #                                        max_pos,
+        #                                        byref(real_unit), 0)
+        # self.max_position_device = real_unit.value
 
         # Get the unit of the motor (mm or deg).
         self.unit_type = self.lib.CC_GetMotorTravelMode(self.serial_number)
@@ -108,40 +119,33 @@ class ThorlabsCubeMotorKinesis(Service):
         elif self.unit_type == 2:
             self.unit = 'deg'
 
-        # Read min, max, unit and model from service configuration.
-        self.min_position_config = self.config['min_position']
-        self.max_position_config = self.config['max_position']
-        unit_config = self.config['unit']
-
-        firmware_version = c_ulong()
-        hardware_version = c_ushort()
-        modification_state = c_ushort()
-        typ = c_ushort()
-        num_channels = c_ushort()
-        model_size = TLI_HardwareInformation.modelNumber.size
-        model = create_string_buffer(model_size)
-        notes_size = TLI_HardwareInformation.notes.size
-        notes = create_string_buffer(notes_size)
-
-        self.lib.CC_GetHardwareInfo(self.serial_number, model, model_size, byref(typ), byref(num_channels),
-                         notes, notes_size, byref(firmware_version), byref(hardware_version),
-                         byref(modification_state))
-
         # Compare the device parameters to the service configuration.
-        if not (self.min_position_device >= self.min_position_config and
-                self.max_position_device <= self.max_position_config and
-                self.cube_model == model.decode() and
-                self.unit == unit_config):
+        if (self.min_position_device < self.min_position_config or
+            self.max_position_device > self.max_position_config or
+            self.unit != unit_config):
+            self.log.debug("Device parameters: min=%f, max=%f, unit=%s",
+                           self.min_position_device, self.max_position_device, self.unit)
+            self.log.debug("Config parameters: min=%f, max=%f, unit=%s",
+                           self.min_position_config, self.max_position_config, unit_config)
+            self.log.debug("Device parameters don't match configuration parameters %r %r %r.",
+                           self.min_position_device < self.min_position_config,
+                           self.max_position_device > self.max_position_config,
+                           self.unit != unit_config)
             raise ValueError("Device parameters don't match configuration parameters.")
 
         self.command = self.make_data_stream('command', 'float64', [1], 20)
         self.current_position = self.make_data_stream('current_position', 'float64', [1], 20)
-        # Submit motor starting position to current_position data stream
-        self.get_current_position()
 
         self.make_command('home', self.home)
         self.make_command('stop', self.stop)
         self.make_command('is_in_motion', self.is_in_motion)
+
+        # wait for completion
+        while self.get_led_switches() != 11:
+            time.sleep(0.1)
+
+        # Submit motor starting position to current_position data stream
+        self.get_current_position()
 
     def main(self):
         while not self.should_shut_down:
@@ -150,7 +154,7 @@ class ThorlabsCubeMotorKinesis(Service):
                 frame = self.command.get_next_frame(10)
                 # Set the current position if a new command has arrived.
                 self.set_current_position(frame.data[0])
-            except Exception:
+            except RuntimeError:
                 # Timed out. This is used to periodically check the shutdown flag.
                 continue
 
@@ -175,8 +179,9 @@ class ThorlabsCubeMotorKinesis(Service):
                                             0)
             self.lib.CC_MoveToPosition(self.serial_number, new_pos_dev)
         else:
-            self.log.warning(f'Motor not moving since commanded position is outside of configured range.')
-            self.log.warning(f'Position limits: {self.min_position_config} {self.unit} <= position <= {self.max_position_config} {self.unit}.')
+            self.log.warning('Motor not moving since commanded position is outside of configured range.')
+            self.log.warning('Position limits: %f %s <= position <= %f %s.',
+                             self.min_position_config, self.unit, self.max_position_config, self.unit)
 
         # Update the current position data stream.
         self.get_current_position()
@@ -185,7 +190,18 @@ class ThorlabsCubeMotorKinesis(Service):
         current_position = self.lib.CC_GetPosition(self.serial_number)
         real_unit = c_double()
         self.lib.CC_GetRealValueFromDeviceUnit(self.serial_number, current_position, byref(real_unit), 0)
+        self.log.debug("Current position: %f %s", real_unit.value, self.unit)
         self.current_position.submit_data(np.array([real_unit.value], dtype='float64'))
+
+    def wait_for_completion(self):
+        # wait for completion
+        message_type = c_ushort()
+        message_id = c_ushort()
+        message_data = c_ulong()
+        while message_id.value != 0 or message_type.value != 2:
+            self.lib.CC_WaitForMessage(self.serial_number, byref(message_type), byref(message_id), byref(message_data))
+        self.lib.CC_ClearMessageQueue(self.serial_number)
+        return message_type.value, message_id.value, message_data.value
 
     def home(self):
         """
@@ -199,15 +215,8 @@ class ThorlabsCubeMotorKinesis(Service):
         self.log.info("Device %s homing\r\n", self.serial_number)
 
         # wait for completion
-        message_type = c_ushort()
-        message_id = c_ushort()
-        message_data = c_ulong()
-        while message_id.value != 0 or message_type.value != 2:
-            self.lib.CC_WaitForMessage(self.serial_number, byref(message_type), byref(message_id), byref(message_data))
-            self.log.info(message_id, message_type)
-
+        self.wait_for_completion()
         self.log.info("Homed - motor %s", self.serial_number)
-        self.lib.CC_ClearMessageQueue(self.serial_number)
 
         # Update the current position data stream.
         self.get_current_position()
@@ -219,13 +228,30 @@ class ThorlabsCubeMotorKinesis(Service):
 
         while self.is_in_motion:
             self.sleep(0.1)
+
         # Update the current position data stream.
         self.get_current_position()
 
+    def get_led_switches(self):
+        """Get the LED indicator bits on cube.
+
+        Returns
+        -------
+        :class:`int`
+            Sum of: 8 to indicate moving 2 to indicate end of track and 1
+            to flash on identify command.
+        """
+        return self.lib.CC_GetLEDswitches(self.serial_number)
+    
     def is_in_motion(self):
-        """Check if the motor is currently moving."""
+        """Check if the motor is currently moving.
+
+        self.sdk.CC_GetLEDswitches(self._serial) returns:
+            Sum of: 8 to indicate moving 2 to indicate end of track and 1
+            to flash on identify command.
+        """
         # TODO return self.motor.is_in_motion, return False always for now
-        return False
+        return self.get_led_switches() == 11111111
 
 
 if __name__ == '__main__':
