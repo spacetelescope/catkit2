a94efcd1f17979ca88a891f4658f0f6d4e7a0144
makidonlab <makidonlabs@gmail.com>
2024-08-12 17:25:21 -0400

    Move properties to proxy - proxy side

--- catkit2/testbed/proxies/thorlabs_mcls1.py
+++ catkit2/testbed/proxies/thorlabs_mcls1.py
@@ -3,9 +3,13 @@ from ..service_proxy import ServiceProxy
 @ServiceProxy.register_service_interface('thorlabs_mcls1')
 class ThorlabsMcls1(ServiceProxy):
     @property
+    def channel(self):
+        return self.config['channel']
+
+    @property
     def center_wavelength(self):
-        return self.wavelength
+        return self.config['channels'][str(self.channel)]
 
     @property
     def bandwidth(self):
-        return self.bandwidth
+        return self.config['bandwidth']
