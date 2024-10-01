740bf7bc31a818ab30df7d358fa16b9b36b34afe
Emiel Por <epor@stsci.edu>
2024-10-01 16:37:17 -0400

    Make sure voltages on the data streams are actually voltages.

--- catkit2/services/bmc_deformable_mirror_sim/bmc_deformable_mirror_sim.py
+++ catkit2/services/bmc_deformable_mirror_sim/bmc_deformable_mirror_sim.py
@@ -1,15 +1,10 @@
 from catkit2.base_services.bmc_deformable_mirror import BmcDeformableMirror
-import numpy as np
 import threading
 
 
 class BmcDeformableMirrorSim(BmcDeformableMirror):
     def __init__(self):
         super().__init__('bmc_deformable_mirror_sim')
-
-        self.flat_map_command = np.zeros(self.num_actuators * self.num_dms)
-        self.gain_map_command = np.zeros(self.num_actuators * self.num_dms)
-        self.gain_map_inv_command = np.zeros(self.num_actuators * self.num_dms)  # TODO: Why are these initializations not needed in the hardware service?
 
         self.lock = threading.Lock()
 
