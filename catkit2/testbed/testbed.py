8790269ab805d4c6b57ed28b51014fb6fc570e9a
Emiel Por <epor@stsci.edu>
2024-11-02 16:46:07 -0700

    Use older variant of the API call.
    This makes this also work on Python 3.8.

--- catkit2/testbed/testbed.py
+++ catkit2/testbed/testbed.py
@@ -278,7 +278,7 @@ class Testbed:
 
         # Read in service types.
         self.service_type_paths = {}
-        for entry_point in importlib_metadata.entry_points(group="catkit2.services"):
+        for entry_point in importlib_metadata.entry_points()["catkit2.services"]:
             module = entry_point.module
             spec = importlib.util.find_spec(module)
             path = os.path.abspath(spec.origin)
