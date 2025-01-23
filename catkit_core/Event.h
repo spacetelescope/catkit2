6a408ddf3333fd17a32fd37bfc81cd0bff785a74
Emiel Por <epor@stsci.edu>
2025-01-23 00:34:09 -0800

    Implement a lock guard for events.

--- catkit_core/Event.h
+++ catkit_core/Event.h
@@ -3,10 +3,29 @@
 
 #include "EventBase.h"
 
+template <typename Event>
+class EventLockGuard
+{
+public:
+	inline EventLockGuard(Event &event)
+		: m_Event(event)
+	{
+		m_Event->Lock();
+	}
+
+	inline ~EventLockGuard()
+	{
+		m_Event->Unlock();
+	}
+
+private:
+	Event &m_Event;
+};
+
 // Select which implementation to use based on the platform.
 #ifdef _WIN32
 using Event = EventImpl<EventImplementationType::ET_SEMAPHORE>;
-#else
+#elif defined(__linux__) or defined(__APPLE__)
 using Event = EventImpl<EventImplementationType::ET_CONDITION_VARIABLE>;
 #endif
 
