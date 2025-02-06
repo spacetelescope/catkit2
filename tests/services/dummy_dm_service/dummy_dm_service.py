ab9d0c390d9ff67c6f18cd140f45aeafc62691b5
Iva Laginja <iva.laginja@obspm.fr>
2025-02-06 15:16:23 +0100

    This should work too

--- tests/services/dummy_dm_service/dummy_dm_service.py
+++ tests/services/dummy_dm_service/dummy_dm_service.py
@@ -9,13 +9,11 @@ class DummyDmService(Service):
 
         self.channel_names = ['correction_howfs', 'correction_lowfs', 'aberration', 'atmosphere']
 
-        self.dm_shape = self.config['dm_shape']  # TODO
-
     def open(self):
         # Make channels streamable
         for channel in self.channel_names:
-            setattr(self, channel, self.make_data_stream(channel, 'float64', [self.dm_shape], 20))  # TODO
-            getattr(self, channel).submit_data(np.zeros(self.dm_shape,))  # TODO
+            setattr(self, channel, self.make_data_stream(channel, 'float64', [self.dm_shape], 20))
+            getattr(self, channel).submit_data(np.zeros(self.dm_shape,))
 
     def main(self):
         while not self.should_shut_down:
