757e5fa7c3d3389fa648027c43c50f9e9502dc90
Iva Laginja <iva.laginja@obspm.fr>
2025-02-12 13:22:09 +0100

    Catch PCIe device and DM opening errors separately

--- catkit2/services/bmc_deformable_mirror_hardware/bmc_deformable_mirror_hardware.py
+++ catkit2/services/bmc_deformable_mirror_hardware/bmc_deformable_mirror_hardware.py
@@ -32,13 +32,13 @@ class BmcDeformableMirrorHardware(BmcDeformableMirror):
     def open(self):
         self.device = bmc.BmcDm()
 
-        status1 = self.device.set_device_id(self.device_id)
-        if status1 != None:
-            pass
+        pcie_status = self.device.set_device_id(self.device_id)
+        if pcie_status != bmc.NO_ERR:
+            raise RuntimeError(f'Failed to connect: {self.dm.error_string(pcie_status)}.')
 
-        status = self.device.open_dm(self.serial_number)
-        if status != bmc.NO_ERR:
-            raise RuntimeError(f'Failed to connect: {self.dm.error_string(status)}.')
+        dm_status = self.device.open_dm(self.serial_number)
+        if dm_status != bmc.NO_ERR:
+            raise RuntimeError(f'Failed to connect: {self.dm.error_string(dm_status)}.')
 
         self.device.enable_high_res(enable=self.enable_high_resolution)
         self.device_command_length = self.device.num_actuators()
