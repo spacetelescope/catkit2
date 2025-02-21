ab2aac4cc24eecc4695f2bd3ee39ea09532c3f27
ehpor <epor@stsci.edu>
2025-02-20 20:24:20 -0800

    Add Event to Shareable::Open().

--- catkit_core/Shareable.cpp
+++ catkit_core/Shareable.cpp
@@ -3,32 +3,28 @@
 #include <stdexcept>
 
 #include "DataStream.h"
-#include "EventConditionVariable.h"
-#include "EventSemaphore.h"
+#include "Event.h"
 #include "FreeListAllocator.h"
-#include "LocalMemory.h"
 #include "PoolAllocator.h"
 #include "SharedMemory.h"
 
-std::unique_ptr<Shareable> Open(void *memory_block)
+std::unique_ptr<Shareable> Shareable::Open(void *memory_block)
 {
 	auto type = *reinterpret_cast<ShareableType *>(memory_block);
-	auto ptr = memory_block + sizeof(ShareableType);
+	auto ptr = (char *) memory_block + sizeof(ShareableType);
 
 	switch (type)
 	{
 	case ShareableType::DataStream:
 		return DataStream::Open(reinterpret_cast<DataStream::SharedState *>(ptr));
-	case ShareableType::EventConditionVariable:
-		return EventConditionVariable::Open(reinterpret_cast<EventConditionVariable::SharedState *>(ptr));
-	case ShareableType::EventSemaphore:
-		return EventSemaphore::Open(reinterpret_cast<EventSemaphore::SharedState *>(ptr));
-	case ShareableType::FreeListAllocator:
+	case ShareableType::Event:
+		return Event::Open(reinterpret_cast<Event::SharedState *>(ptr));
+	/*case ShareableType::FreeListAllocator:
 		return FreeListAllocator::Open(reinterpret_cast<FreeListAllocator::SharedState *>(ptr));
 	case ShareableType::LocalMemory:
 		return LocalMemory::Open(reinterpret_cast<LocalMemory::SharedState *>(ptr));
 	case ShareableType::PoolAllocator:
-		return PoolAllocator::Open(reinterpret_cast<PoolAllocator::SharedState *>(ptr));
+		return PoolAllocator::Open(reinterpret_cast<PoolAllocator::SharedState *>(ptr));*/
 	case ShareableType::SharedMemory:
 		return SharedMemory::Open(reinterpret_cast<SharedMemory::SharedState *>(ptr));
 	default:
