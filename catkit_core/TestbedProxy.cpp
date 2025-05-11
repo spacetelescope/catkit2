1b101831df00931c6aca7e06ec2dce56f1c6cb3d
Emiel Por <epor@stsci.edu>
2025-05-11 10:28:51 -0700

    Add broker to testbed and testbed proxy.

--- catkit_core/TestbedProxy.cpp
+++ catkit_core/TestbedProxy.cpp
@@ -373,6 +373,10 @@ void TestbedProxy::GetTestbedInfo()
 
 	m_HeartbeatStream = DataStream::Open(reply.heartbeat_stream_id());
 
+	m_MessageBrokerHeader = SharedMemory::Open(reply.message_broker_id());
+	StructStream stream = StructStream(m_MessageBrokerHeader->GetAddress());
+	m_MessageBroker = MessageBroker::Open(stream);
+
 	m_LoggingIngressPort = reply.logging_ingress_port();
 	m_LoggingEgressPort = reply.logging_egress_port();
 
