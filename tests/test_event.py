7adb6da4e12b870117d6458c78e67d9b817defdd
Emiel Por <epor@ucsc.edu>
2025-05-17 20:25:02 -0700

    Test that we are waiting for at least the timeout duration.

--- tests/test_event.py
+++ tests/test_event.py
@@ -37,8 +37,13 @@ def test_event(wait_method):
     assert waiting.is_set(), 'Something went wrong starting the waiting thread.'
 
     # Ensure the event is not triggered unless signaled.
+    start = time.perf_counter()
     with pytest.raises(RuntimeError):
         event.wait(lambda: condition[0] != 0, 0.1)
+    end = time.perf_counter()
+
+    # Ensure that we at least waited for the timeout duration.
+    assert (end - start) >= 0.1
 
     # Signal the event.
     condition[0] = 1
