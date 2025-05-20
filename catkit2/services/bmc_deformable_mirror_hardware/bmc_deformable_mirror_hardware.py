6c1f1b92d48b3dc7b44bc271d93d5dba8d4e7a09
Sarah Steiger <ssteiger@stsci.edu>
2025-05-20 10:53:07 -0400

    swap self.dm to self.device in RuntimeErrors

--- catkit2/services/bmc_deformable_mirror_hardware/bmc_deformable_mirror_hardware.py
+++ catkit2/services/bmc_deformable_mirror_hardware/bmc_deformable_mirror_hardware.py
@@ -31,11 +31,11 @@ class BmcDeformableMirrorHardware(BmcDeformableMirror):
 
         pcie_status = self.device.set_device_id(self.device_id)
         if pcie_status != bmc.NO_ERR:
-            raise RuntimeError(f'Failed to connect: {self.dm.error_string(pcie_status)}.')
+            raise RuntimeError(f'Failed to connect: {self.device.error_string(pcie_status)}.')
 
         dm_status = self.device.open_dm(self.serial_number)
         if dm_status != bmc.NO_ERR:
-            raise RuntimeError(f'Failed to connect: {self.dm.error_string(dm_status)}.')
+            raise RuntimeError(f'Failed to connect: {self.device.error_string(dm_status)}.')
 
         self.device.enable_high_res(enable=self.enable_high_resolution)
         self.device_command_length = self.device.num_actuators()
