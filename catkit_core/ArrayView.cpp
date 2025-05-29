0cc5f2c4e66b222c622399816587f5feefadb04a
Emiel Por <epor@stsci.edu>
2025-05-29 16:23:04 -0700

    Fixes due to changes in ArrayView structure.

--- catkit_core/ArrayView.cpp
+++ catkit_core/ArrayView.cpp
@@ -1,4 +1,4 @@
-#include "Array.h"
+#include "ArrayView.h"
 
 #include <cstring>
 
@@ -49,73 +49,17 @@ std::size_t ArrayInfo::GetSizeInBytes() const
 	return GetSize() * item_size;
 }
 
-Array::Array()
-	: data(nullptr), owns_data(false)
-{
-}
-
-Array::Array(const ArrayInfo &info, void *data, bool owns_data)
-	: data(data), info(info), owns_data(owns_data)
-{
-}
-
-Array::Array(const Array &other)
-	: data(other.data), info(other.info), owns_data(false)
-{
-}
-
-Array::Array(Array &&other) noexcept
-	: Array()
-{
-	*this = std::move(other);
-}
-
-Array::~Array()
-{
-	if (owns_data)
-		delete[] data;
-}
-
-Array &Array::operator=(const Array &other)
-{
-	info = other.info;
-	data = other.data;
-	owns_data = false;
-
-	return *this;
-}
-
-Array &Array::operator=(Array &&other) noexcept
-{
-	std::swap(info, other.info);
-	std::swap(data, other.data);
-	std::swap(owns_data, other.owns_data);
-
-	return *this;
-}
-
-Array Array::Copy()
-{
-	if (!data)
-		return {};
-
-	char *new_data = new char[info.GetSizeInBytes()];
-	std::memcpy(new_data, data, info.GetSizeInBytes());
-
-	return Array(info, new_data);
-}
-
-bool Array::IsAligned() const
+bool ArrayView::IsAligned() const
 {
 	return uintptr_t(data) % 128 == 0;
 }
 
-bool Array::IsCContiguous() const
+bool ArrayView::IsCContiguous() const
 {
 	return info.IsCContiguous();
 }
 
-bool Array::IsFContiguous() const
+bool ArrayView::IsFContiguous() const
 {
 	return info.IsFContiguous();
 }
