93cbd63114a409b0b57e3ed462132e3ca7525d52
soummer <soummer@stsci.edu>
2025-05-30 15:24:15 -0400

    Update to the existing test

--- tests/test_shared_memory.py
+++ tests/test_shared_memory.py
@@ -6,9 +6,10 @@ def test_shared_memory_lifetime():
     stream_id = 'test_memory'
     memory = SharedMemory.create(stream_id, 1024)
 
-    # Creating shared memory with the same ID should raise en erorr.
-    with pytest.raises(RuntimeError):
+    # Creating shared memory with the same ID should raise an error.
+    with pytest.raises(RuntimeError) as excinfo:
         SharedMemory.create(stream_id, 1024)
+    assert "File exists" in str(excinfo.value)
 
     # We should be able to access the shared memory when opening.
     memory2 = SharedMemory.open(stream_id)
@@ -23,8 +24,10 @@ def test_shared_memory_lifetime():
     del memory2
 
     # Now we should not be able to open the shared memory anymore.
-    with pytest.raises(RuntimeError):
+    with pytest.raises(RuntimeError) as excinfo:
         SharedMemory.open(stream_id)
+    assert "No such file or directory" in str(excinfo.value)
 
     # However, we should be able to create a new shared memory with the same ID.
     memory = SharedMemory.create(stream_id, 1024)
+    del memory
