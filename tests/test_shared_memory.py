29af600f4fe96ffd2e11e0ae8eeecbfafac1cfe0
soummer <soummer@stsci.edu>
2025-05-30 16:02:12 -0400

    updated to use match and typo fix

--- tests/test_shared_memory.py
+++ tests/test_shared_memory.py
@@ -7,9 +7,8 @@ def test_shared_memory_lifetime():
     memory = SharedMemory.create(stream_id, 1024)
 
     # Creating shared memory with the same ID should raise an error.
-    with pytest.raises(RuntimeError) as excinfo:
+    with pytest.raises(RuntimeError, match="Something went wrong while creating shared memory"):
         SharedMemory.create(stream_id, 1024)
-    assert "File exists" in str(excinfo.value)
 
     # We should be able to access the shared memory when opening.
     memory2 = SharedMemory.open(stream_id)
@@ -24,9 +23,8 @@ def test_shared_memory_lifetime():
     del memory2
 
     # Now we should not be able to open the shared memory anymore.
-    with pytest.raises(RuntimeError) as excinfo:
+    with pytest.raises(RuntimeError, match="Something went wrong while opening shared memory"):
         SharedMemory.open(stream_id)
-    assert "No such file or directory" in str(excinfo.value)
 
     # However, we should be able to create a new shared memory with the same ID.
     memory = SharedMemory.create(stream_id, 1024)
@@ -47,23 +45,20 @@ def test_shared_memory_open_non_existent_or_invalid_name():
     stream_id = 'non_existent_memory'
 
     # Opening a non-existent shared memory should raise an error.
-    with pytest.raises(RuntimeError) as excinfo:
+    with pytest.raises(RuntimeError, match="Something went wrong while opening shared memory"):
         SharedMemory.open(stream_id)
-    assert "Something went wrong while opening shared memory" in str(excinfo.value)
 
     # Opening a shared memory with an invalid name should raise an error.
-    with pytest.raises(RuntimeError) as excinfo:
+    with pytest.raises(RuntimeError, match="Something went wrong while creating shared memory"):
         SharedMemory.create("", 1024)  # Invalid name
-    assert "Something went wrong while creating shared memory" in str(excinfo.value)
-
 
 def test_shared_memory_wrong_type():
     # Opening a shared memory with a float for the memory size should raise an error.
     with pytest.raises(TypeError) as excinfo:
-        SharedMemory.create('dflgkjdhfg', 1024.1)  # Invalid name
+        SharedMemory.create('some_id', 1024.1)  # Invalid type
     assert "create(): incompatible function arguments" in str(excinfo.value)
 
     # Opening a shared memory with a string for the memory size should raise an error.
     with pytest.raises(TypeError) as excinfo:
-        SharedMemory.create('dflgkjdhfg', 'abc')  # Invalid name
+        SharedMemory.create('some_id', 'abc')  # Invalid type
     assert "create(): incompatible function arguments" in str(excinfo.value)
