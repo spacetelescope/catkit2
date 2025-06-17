677a02e8f575b5c8837550bbee462bcd67476312
THD2team <thd2team@gmail.com>
2025-06-17 14:55:45 +0200

    temperature cooling on

--- catkit2/services/hamamatsu_camera/hamamatsu_camera.py
+++ catkit2/services/hamamatsu_camera/hamamatsu_camera.py
@@ -97,6 +97,9 @@ class HamamatsuCamera(Service):
         if self.cam.dev_open() is False:
             raise RuntimeError(f'Dcam.dev_open() fails with error {self.cam.lasterr()}')
 
+        # Set temperature cooling to "ON" (tarder =-20)
+        self.cam.prop_setvalue(dcam.DCAM_IDPROP.SENSORCOOLER, 2.0)
+
         # Set subarray mode to on so that it checks subarray compatibility when picking ROI
         self.cam.prop_setvalue(dcam.DCAM_IDPROP.SUBARRAYMODE, 2.0)
 
