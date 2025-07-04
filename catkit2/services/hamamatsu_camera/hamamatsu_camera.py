1758a4355ffcf1b965a18bfa1259a14333cc2c42
Corentin Paviot <corentin.paviot@institutoptique.fr>
2025-07-04 14:28:31 +0200

    undo unwanting deleted line

--- catkit2/services/hamamatsu_camera/hamamatsu_camera.py
+++ catkit2/services/hamamatsu_camera/hamamatsu_camera.py
@@ -97,6 +97,9 @@ class HamamatsuCamera(Service):
         if self.cam.dev_open() is False:
             raise RuntimeError(f'Dcam.dev_open() fails with error {self.cam.lasterr()}')
 
+        # Set subarray mode to on so that it checks subarray compatibility when picking ROI
+        self.cam.prop_setvalue(dcam.DCAM_IDPROP.SUBARRAYMODE, 2.0)
+
         # Set water cooling mode and fan mode
         self.cooling_mode = self.config.get('cooling_mode', 'on')
         self.fan_on = self.config.get('fan_on', True)
