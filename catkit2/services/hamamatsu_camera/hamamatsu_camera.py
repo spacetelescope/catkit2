54efad7439c84dd6ec07ee6a60c85f043490ffd4
THD2team <thd2team@gmail.com>
2025-07-07 15:51:31 +0200

    correct values

--- catkit2/services/hamamatsu_camera/hamamatsu_camera.py
+++ catkit2/services/hamamatsu_camera/hamamatsu_camera.py
@@ -101,7 +101,7 @@ class HamamatsuCamera(Service):
         self.cooling_mode = self.config.get('cooling_mode', 'on')
         self.fan_on = self.config.get('fan_on', True)
 
-        fan = 1 if self.fan_on else 0
+        fan = 2.0 if self.fan_on else 1.0
         self.cam.prop_setvalue(dcam.DCAM_IDPROP.SENSORCOOLERFAN, fan)
 
         if self.cooling_mode == 'off':
