0d53d4c7086d8972a96c764cac36c67cab3725ff
Axel Potier <axel.potier@obspm.fr>
2025-07-21 16:13:19 +0200

    Remove pulse picker ratio proxy

--- catkit2/testbed/proxies/nkt_superk.py
+++ catkit2/testbed/proxies/nkt_superk.py
@@ -84,22 +84,3 @@ class NktSuperkProxy(ServiceProxy):
         self.power_setpoint.submit_data(np.array([power_to_set], dtype='float32'))
 
 
-    @property
-    def pulse_picker_value(self):
-        print('Getting pulse_picker_ratio value...')
-        try:
-            return self.pulse_picker_ratio.get()[0]
-        except RuntimeError:
-                # The frame wasn't available anymore because we were waiting too long.
-                print('Could not getting pulse picker ratio')
-
-    @pulse_picker_value.setter
-    def pulse_picker_value(self, pulse_picker_ratio_to_set):
-        print('Setting pulse_picker_ratio value...')
-        try:
-            self.pulse_picker_ratio.submit_data(np.array([pulse_picker_ratio_to_set], dtype='uint16'))
-        except RuntimeError:
-                # The frame wasn't available anymore because we were waiting too long.
-                print('Could not setting pulse picker ratio')
-
-
