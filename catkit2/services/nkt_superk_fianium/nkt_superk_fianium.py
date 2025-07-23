93cbe9141d4e418b246b613b378180f47e6b3102
Iva Laginja <iva.laginja@obspm.fr>
2025-07-23 11:49:48 +0200

    Add safe pulse picker ratios for open() and close()

--- catkit2/services/nkt_superk_fianium/nkt_superk_fianium.py
+++ catkit2/services/nkt_superk_fianium/nkt_superk_fianium.py
@@ -113,7 +113,7 @@ class NktSuperkFianium(Service):
         # once the monitor threads have started.
         self.emission.submit_data(np.array([self.config['emission']], dtype='uint8'))
         self.power_setpoint.submit_data(np.array([self.config['power_setpoint']], dtype='float32'))
-        self.pulse_picker_ratio.submit_data(np.array([self.config['pulse_picker_ratio']], dtype='uint16'))
+        self.pulse_picker_ratio.submit_data(np.array([100], dtype='uint16'))  # Picking a safe default value of 100.
 
         self.nd_setpoint.submit_data(np.array([self.config['nd_setpoint']], dtype='float32'))
         self.swp_setpoint.submit_data(np.array([self.config['swp_setpoint']], dtype='float32'))
@@ -151,7 +151,7 @@ class NktSuperkFianium(Service):
             self.sleep(1)
 
     def close(self):
-        # TODO: Set pulse picker ratio to a safe value before closing?
+        self.pulse_picker_ratio.submit_data(np.array([100], dtype='uint16'))
 
         # Turn off the source
         self.set_emission(0)
