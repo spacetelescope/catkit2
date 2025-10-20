c014493c806235e0d758a86fb3c62d4ad15332d2
Sarah Steiger <ssteiger@stsci.edu>
2025-10-20 15:45:29 -0400

    add disclaimer to post_gen_project.py

--- cookiecutter-testbed/hooks/post_gen_project.py
+++ cookiecutter-testbed/hooks/post_gen_project.py
@@ -2,3 +2,6 @@
 
 if __name__ == "__main__":
     print("Your Python package project has been created successfully!")
+    print("Note: Your Python package will be generated with defaults "
+          "(including licenses!). Make sure to check all files before making "
+          "your package publicly available.")
