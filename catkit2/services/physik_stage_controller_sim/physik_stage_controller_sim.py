f80445ebd833d06428075fd68d45f31113ea1bd3
MichaelPhilbin <michaelphilbinn@gmail.com>
2026-02-12 15:59:08 -0800

    changed move_to_strem back to FIRST_ONLY

--- catkit2/services/physik_stage_controller_sim/physik_stage_controller_sim.py
+++ catkit2/services/physik_stage_controller_sim/physik_stage_controller_sim.py
@@ -8,7 +8,6 @@ but doesn't require actual hardware. Useful for testing and development.
 import numpy as np
 import threading
 from catkit2.testbed.service import Service
-from catkit2.catkit_bindings import BufferHandlingMode
 import time
 
 
@@ -64,9 +63,6 @@ class PhysikStageControllerSim(Service):
 
         # Create data stream for move_to command
         self.move_to_stream = self.make_data_stream('move_to_stream', 'float64', [num_axes], 20)
-
-         # Set to process all frames in order
-        self.move_to_stream.buffer_handling_mode = BufferHandlingMode.OLDEST_FIRST_OVERWRITE
 
         # Precompute reverse map for speed
         self.axis_num_to_name = {num: name for name, num in self.axis_map.items()}
