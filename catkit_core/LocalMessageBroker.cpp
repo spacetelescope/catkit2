718f0f30b1d2dc46a6c8b6c0dbeef36e114608d2
Emiel Por <emiel.por@gmail.com>
2026-02-14 23:56:25 +0900

    Remove some debug print statements.

--- catkit_core/LocalMessageBroker.cpp
+++ catkit_core/LocalMessageBroker.cpp
@@ -267,7 +267,6 @@ std::size_t LocalMessageBroker::CalculateBufferSize()
 
 Message LocalMessageBroker::PrepareMessageImpl(std::string_view topic, size_t payload_size, Uuid trace_id, uint8_t memory_block_id)
 {
-	std::cerr << "LocalMessageBroker::PrepareMessageImpl" << std::endl;
 	// Allocate a payload.
 	auto allocator = GetAllocator(memory_block_id);
 
@@ -344,15 +343,12 @@ Message LocalMessageBroker::PrepareMessageImpl(std::string_view topic, size_t pa
 	header->num_metadata_entries = 0;
 
 	DEBUG_PRINT("Header set");
-	std::cerr << "Header: " << header->payload_info.total_size << ", " << header->payload_info.memory_block_id << std::endl;
 
 	return Message(header, payload, INVALID_FRAME_ID);
 }
 
 Message LocalMessageBroker::PublishMessage(Message message, bool is_final)
 {
-	std::cerr << "LocalMessageBroker::PublishMessage" << std::endl;
-
 	DEBUG_PRINT("Publishing message.");
 
 	if (message.m_Header == nullptr || message.m_Payload == nullptr)
@@ -581,7 +577,6 @@ ShareableType LocalMessageBroker::GetType() const
 
 std::shared_ptr<HybridPoolAllocator> LocalMessageBroker::GetAllocator(uint8_t memory_block_id)
 {
-	std::cerr << "Number of memory blocks: " << m_Allocators.size() << std::endl;
 	if (memory_block_id >= m_Allocators.size())
 	{
 		return nullptr;
