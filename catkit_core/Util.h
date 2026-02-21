9018bf4237803d7d209231b571a776b85ace410c
Emiel Por <emiel.por@gmail.com>
2026-02-21 14:15:24 +0900

    Add bit operations.
    These functions are in C++20, but we're on C++17 right now and not going to upgrade.

--- catkit_core/Util.h
+++ catkit_core/Util.h
@@ -15,15 +15,26 @@ ProtoClass Deserialize(const std::string &data);
 
 void Sleep(double sleep_time_in_sec, std::function<bool()> cancellation_callback = nullptr);
 
-template <typename UnsignedType>
-constexpr UnsignedType round_up_to_power_of_2(UnsignedType v);
+template <typename T>
+using enable_uint = std::enable_if_t<std::is_unsigned<T>::value, int>;
 
-template <typename UnsignedType>
-constexpr UnsignedType round_down_to_power_of_2(UnsignedType v);
+template <typename T, enable_uint<T> = 0>
+constexpr T round_up_to_power_of_2(T v) noexcept;
+
+template <typename T, enable_uint<T> = 0>
+constexpr T round_down_to_power_of_2(T v) noexcept;
 
 // Cross-platform implementation of std::bit_width() (in absence of C++20)
-template <typename T>
-constexpr int bit_width(T x);
+template <typename T, enable_uint<T> = 0>
+inline constexpr int bit_width(T x) noexcept;
+
+// Cross-platform implementation of std::countr_zero() (in absence of C++20)
+template <typename T, enable_uint<T> = 0>
+inline unsigned countr_zero(T x) noexcept;
+
+// Cross-platform implementation of std::countl_zero() (in absence of C++20)
+template <typename T, enable_uint<T> = 0>
+inline unsigned countl_zero(T x) noexcept;
 
 #include "Util.inl"
 
