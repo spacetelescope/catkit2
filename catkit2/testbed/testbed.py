4e19aec8c5f921e279c42fb85aa356a003a77d6e
Raphaël Pourcelot <rapourcelot@mpia.de>
2026-07-16 12:13:49 +0200

    Fix entry point in testbed

--- catkit2/testbed/testbed.py
+++ catkit2/testbed/testbed.py
@@ -354,7 +354,7 @@ class Testbed:
 
         # Read in service types.
         self.service_type_paths = {}
-        for entry_point in importlib_metadata.entry_points().get("catkit2.services", []):
+        for entry_point in importlib_metadata.entry_points(group="catkit2.services"):  # .get(, []):
             try:
                 # grab the module
                 module = entry_point.module
