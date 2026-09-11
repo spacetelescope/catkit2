8a27a8859652abbcfcdec0d875aeb516adaedd47
Emiel Por <emiel.por@gmail.com>
2026-09-11 11:12:44 -0400

    Use spaces  instead of tabs.

--- catkit2/version.py
+++ catkit2/version.py
@@ -1,26 +1,26 @@
 def get_version():
-	'''Return the version of this package.
+    '''Return the version of this package.
 
-	Returns
-	-------
-	string
-		The version of the catkit2 package.
-	'''
-	if get_version._version is None:
-		try:
-			# Python 3.8+: importlib.metadata is in the standard library.
-			from importlib.metadata import version, PackageNotFoundError
-		except ImportError:
-			# Python 3.7: importlib.metadata is not in the stdlib, use the
-			# importlib_metadata backport.
-			from importlib_metadata import version, PackageNotFoundError
+    Returns
+    -------
+    string
+        The version of the catkit2 package.
+    '''
+    if get_version._version is None:
+        try:
+            # Python 3.8+: importlib.metadata is in the standard library.
+            from importlib.metadata import version, PackageNotFoundError
+        except ImportError:
+            # Python 3.7: importlib.metadata is not in the stdlib, use the
+            # importlib_metadata backport.
+            from importlib_metadata import version, PackageNotFoundError
 
-		try:
-			get_version._version = version('catkit2')
-		except PackageNotFoundError:
-			# package is not installed
-			pass
+        try:
+            get_version._version = version('catkit2')
+        except PackageNotFoundError:
+            # package is not installed
+            pass
 
-	return get_version._version
+    return get_version._version
 
 get_version._version = None
